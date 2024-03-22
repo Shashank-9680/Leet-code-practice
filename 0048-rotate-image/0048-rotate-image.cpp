4397181c2df431bd4a9dda190f5899b52cf8d98f
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-03-22 19:21:47 +0530

    Time: 5 ms (5.96%), Space: 8.8 MB (21.86%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-       int n=matrix.size();
-        int k=0;
+     int n = matrix.size();
         
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-              int t=matrix[i][j];
-                    matrix[i][j]=matrix[j][i];
-                matrix[j][i]=t;
+       
+        vector<vector<int>>nums(n,vector<int>(n,0));
+        
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                nums[j][n-1-i] = matrix[i][j];
             }
-           
-        }
-        int low=0;int high=n-1;
-        while(low<=high){
-           for(int i=0;i<n;i++){
-               int temp= matrix[i][low];
-               matrix[i][low]=matrix[i][high];
-               matrix[i][high]=temp;
-           } 
-            high--;
-            low++;
         }
         
+       
+        matrix= nums;
     }
 };
