d805e9ae347954a35498c4e00a3db16eb8ebd607
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-06-05 23:16:49 +0530

    Time: 21 ms (65.64%), Space: 33.2 MB (32.84%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,22 +1,48 @@
 class Solution {
-public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
-        unordered_map<int, int> prefixSumCount;
-        int currentSum = 0;
+    int count(vector<int>nums,int goal){
+        int n = nums.size();
+        int i = 0;
+        int j = 0;
+        int sum = 0;
         int count = 0;
-
-        prefixSumCount[0] = 1;  
-
-        for (int num : nums) {
-            currentSum += num;
-            if (prefixSumCount.find(currentSum - goal) != prefixSumCount.end()) {
-                count += prefixSumCount[currentSum - goal];
+       if(goal<0){
+           return 0;
+       } 
+        while (j < n) {
+            sum=sum+ nums[j];
+            
+            while (sum > goal) {
+                sum =sum- nums[i];
+                i++;
             }
-            prefixSumCount[currentSum]++;
-        }
-
-        return count;
+            
+            count =count+ (j - i + 1);
+            j++;
         }
         
-    
+        return count;
+    }
+public:
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+//         unordered_map<int, int> prefixSumCount;
+//         int currentSum = 0;
+//         int count = 0;
+
+//         prefixSumCount[0] = 1;  
+
+//         for (int num : nums) {
+//             currentSum += num;
+//             if (prefixSumCount.find(currentSum - goal) != prefixSumCount.end()) {
+//                 count += prefixSumCount[currentSum - goal];
+//             }
+//             prefixSumCount[currentSum]++;
+//         }
+
+//         return count;
+//         }
+        
+    int count1=count(nums,goal);
+        int count2=count(nums,goal-1);
+        return count1-count2;
+    }
 };
