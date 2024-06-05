5f42f8fa5909733b71965f222d7cee34c14a4037
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-06-05 17:46:20 +0530

    Time: 38 ms (72.09%), Space: 58 MB (13.01%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.cpp
@@ -6,17 +6,31 @@ public:
         int count=0;
         int length=0;
         int n=nums.size();
+        // while(j<n){
+        //     if(nums[j]==0){
+        //         count++;
+        //     }
+        //     if(count>k){
+        //         if(nums[i]==0) count--;
+        //         i++;
+        //     }
+        //     if(count<=k) {
+        //         length=max(length,j-i+1);
+        //     }
+        //     j++;
+        // }
         while(j<n){
             if(nums[j]==0){
                 count++;
             }
-            if(count>k){
-                if(nums[i]==0) count--;
+            while(count>k){
+                
+                if(nums[i]==0){
+                    count--;
+                }
                 i++;
             }
-            if(count<=k) {
-                length=max(length,j-i+1);
-            }
+            length=max(length,j-i+1);
             j++;
         }
         return length;
