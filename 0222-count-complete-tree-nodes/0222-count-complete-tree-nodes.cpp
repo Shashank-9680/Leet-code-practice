9a5c6f8c3adbf1fb713bd81d39b3add79bdf1b2f
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-06-14 12:32:25 +0530

    Time: 22 ms (69.12%), Space: 29.2 MB (14.89%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -10,27 +10,49 @@
  * };
  */
 class Solution {
+    int findleftheight(TreeNode*root){
+        int count=0;
+        while(root){
+            count++;
+            root=root->left;
+        }
+        return count;
+    }
+    int findrightheight(TreeNode*root){
+         int count=0;
+        while(root){
+            count++;
+            root=root->right;
+        }
+        return count;
+    }
+    
 public:
     int countNodes(TreeNode* root) {
-        if(root==NULL) return 0;
-        queue<TreeNode*>q;
-        q.push(root);
-        int count=0;
-        while(!q.empty()){
-            TreeNode*temp=q.front();
-            q.pop();
-            if(temp->left!=NULL){
-                count++;
-                q.push(temp->left);
+//         if(root==NULL) return 0;
+//         queue<TreeNode*>q;
+//         q.push(root);
+//         int count=0;
+//         while(!q.empty()){
+//             TreeNode*temp=q.front();
+//             q.pop();
+//             if(temp->left!=NULL){
+//                 count++;
+//                 q.push(temp->left);
                 
-            }
-            if(temp->right!=NULL){
-                 count++;
-                q.push(temp->right);
+//             }
+//             if(temp->right!=NULL){
+//                  count++;
+//                 q.push(temp->right);
                
-            }
+//             }
             
-        }
-        return count+1;
+//         }
+//         return count+1;
+        if(root==NULL) return 0;
+        int lf=findleftheight(root);
+        int rt=findrightheight(root);
+        if(lf==rt) return (1<<lf)-1;
+        return 1+countNodes(root->left)+countNodes(root->right);
     }
 };
