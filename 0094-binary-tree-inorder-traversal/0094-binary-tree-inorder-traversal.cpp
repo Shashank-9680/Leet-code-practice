c07ee141cf8fe5ab17b5b7f26f2c50573d1ebbfd
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-06-15 12:33:41 +0530

    Time: 4 ms (31.56%), Space: 9.6 MB (92.81%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -24,24 +24,48 @@ class Solution {
 // }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
-         vector<int>v;
-        // inOrderTraversal(root,v);
-        TreeNode*temp=root;
-        stack<TreeNode*>st;
-        if(root==NULL) return v;
-    while(true){
-       if(temp!=NULL){     
-           st.push(temp);
-            temp=temp->left;
-       }
-        else{
-            if(st.empty()) break;
-            temp=st.top();
-            st.pop();
-            v.push_back(temp->val);
-            temp=temp->right;
+    //      vector<int>v;
+    //     // inOrderTraversal(root,v);
+    //     TreeNode*temp=root;
+    //     stack<TreeNode*>st;
+    //     if(root==NULL) return v;
+    // while(true){
+    //    if(temp!=NULL){     
+    //        st.push(temp);
+    //         temp=temp->left;
+    //    }
+    //     else{
+    //         if(st.empty()) break;
+    //         temp=st.top();
+    //         st.pop();
+    //         v.push_back(temp->val);
+    //         temp=temp->right;
+    //     }
+    // }
+// return v;
+        vector<int>inorder;
+        TreeNode*curr=root;
+        while(curr!=NULL){
+            if(curr->left==NULL){
+                inorder.push_back(curr->val);
+                curr=curr->right;
+            }
+            else{
+                TreeNode*prev=curr->left;
+                while(prev->right&&prev->right!=curr){
+                  prev=prev->right;
+                }
+                if(prev->right==NULL){
+                    prev->right=curr;
+                    curr=curr->left;
+                }
+                else{
+                    prev->right=NULL;
+                    inorder.push_back(curr->val);
+                    curr=curr->right;
+                }
+            }
         }
-    }
-return v;
+        return inorder;
     }
 };
