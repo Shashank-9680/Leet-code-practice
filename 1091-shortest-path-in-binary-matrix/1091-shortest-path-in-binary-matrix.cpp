ae2aadde4974aa6f2ebac24f1f9f39c53c46fc52
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-06-24 23:29:06 +0530

    Time: 49 ms (52.42%), Space: 24.9 MB (47.26%) - LeetHub

---
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>v(n,vector<int>(m,INT_MAX));
+        queue<pair<int,pair<int,int>>>q;
+        q.push({1,{0,0}});
+        v[0][0]=1;
+            if (grid[0][0] != 0 || grid[n-1][m-1] != 0) return -1;
+        while(!q.empty()){
+            int weight=q.front().first;
+            int node1=q.front().second.first;
+            int node2=q.front().second.second;
+            q.pop();
+            
+       for(int nr=-1;nr<=1;nr++){
+           for(int nc=-1;nc<=1;nc++){
+               int nrow=node1+nr;
+               int ncol=node2+nc;
+               if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&grid[nrow][ncol]==0){
+                   if(1+weight<v[nrow][ncol]){
+                       v[nrow][ncol]=1+weight;
+                       q.push({v[nrow][ncol],{nrow,ncol}});
+                   }
+               }
+           }
+       
+       }     
+        }
+         return (v[n-1][m-1] == INT_MAX) ? -1 : v[n-1][m-1];
+       
+    }
+};
