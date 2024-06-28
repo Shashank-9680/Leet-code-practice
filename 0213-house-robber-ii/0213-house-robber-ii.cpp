ab31b0a7ea57c45cd1e2a3e67065bf0965aee533
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-06-28 19:02:12 +0530

    Time: 5 ms (6.25%), Space: 9.5 MB (11.89%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,13 +1,25 @@
 class Solution {
     int maximum(vector<int>& dp, vector<int>& nums, int n) {
-        if (n == 0) return nums[0];
-        if (n < 0) return 0;
-        if (dp[n] != -1) return dp[n];
+//         if (n == 0) return nums[0];
+//         if (n < 0) return 0;
+//         if (dp[n] != -1) return dp[n];
         
-        int pick = nums[n] + maximum(dp, nums, n - 2);
-        int notpick = 0 + maximum(dp, nums, n - 1);
+//         int pick = nums[n] + maximum(dp, nums, n - 2);
+//         int notpick = 0 + maximum(dp, nums, n - 1);
         
-        return dp[n] = max(pick, notpick);
+//         return dp[n] = max(pick, notpick);
+        dp[0]=nums[0];
+        for(int i=1;i<n;i++){
+           int pick = nums[i];
+        if (i > 1)
+            pick += dp[i - 2];
+        int nonPick = dp[i - 1];
+        
+        // Store the maximum value in the dp array
+        dp[i] = max(pick, nonPick);
+            
+        }
+        return dp[n-1];
     }
 public:
     int rob(vector<int>& nums) {
@@ -20,10 +32,10 @@ public:
             if (i != n - 1) temp2.push_back(nums[i]);
         }
         
-        vector<int> dp1(n - 1, -1);
-        vector<int> dp2(n - 1, -1);
+        vector<int> dp1(n - 1);
+        vector<int> dp2(n - 1);
         
-        return max(maximum(dp1, temp1, n - 2), maximum(dp2, temp2, n - 2));
+        return max(maximum(dp1, temp1, n -1), maximum(dp2, temp2, n - 1));
        
         
     }
