241f18c517f7efa1b375f68e6036920616d88ed0
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-01 01:55:36 +0530

    Time: 124 ms (41.45%), Space: 38.4 MB (22.69%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,32 +1,34 @@
 class Solution {
-    int solve(int ind,int target,vector<int> &nums,map<pair<int,int>,int> &dp)
-{
-   
-    
-    if(ind < 0)
-    {
-        //either you achieve the target or you don't achieve the target
-        if(target == 0)
-            return 1;
-        else
-            return 0; 
+   int count(int n, int sum, vector<int>& arr, map<pair<int, int>, int>& dp) {
+        // Base case
+        if (n == 0) {
+            if (sum == 0 && arr[0] == 0) return 2;
+            if (arr[0] == sum || sum == 0) return 1;
+            return 0;
+        }
+        
+        // Check if the result is already computed
+        if (dp.find({n, sum}) != dp.end()) return dp[{n, sum}];
+        
+        // Do not pick the current element
+        int notpick = count(n - 1, sum, arr, dp);
+        
+        // Pick the current element
+        int pick = 0;
+        if (arr[n] <= sum) {
+            pick = count(n - 1, sum - arr[n], arr, dp);
+        }
+        
+        // Store the result in the map and return
+        return dp[{n, sum}] = (pick + notpick) ;
     }
-    if(dp.find({ind, target}) != dp.end())
-    {
-        return dp[{ind,target}];
-    }
-    int plus = 0;
-    int minus = 0;
-    plus = solve(ind-1,target + nums[ind],nums,dp);
-    minus = solve(ind - 1, target - nums[ind],nums,dp);
-
-    return dp[{ind, target}]  = plus + minus;
-    
-
-}
 public:
     int findTargetSumWays(vector<int>& arr, int d) {
           map<pair<int,int>,int> dp;
-        return solve(arr.size()-1,d,arr,dp);
+         int sum = accumulate(arr.begin(), arr.end(), 0);
+        if ((sum + d) % 2 != 0 || d > sum) return 0;
+         
+        int target = (sum + d) / 2;
+        return count(arr.size()-1,target,arr,dp);
     }
 };
