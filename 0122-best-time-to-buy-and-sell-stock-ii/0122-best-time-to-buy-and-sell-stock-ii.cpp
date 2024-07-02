4b8ad1c0510690a415c6e1710c7d6ac08b199b17
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-02 18:34:09 +0530

    Time: 7 ms (42.97%), Space: 15.5 MB (48.59%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,24 +1,40 @@
 class Solution {
-    int maximum(int i,int j,vector<int>&prices,vector<vector<int>>&dp,int n){
-        if(i==n){
-            return 0;
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(j==1){
-           int take=-prices[i]+maximum(i+1,0,prices,dp,n);
-            int nottake=maximum(i+1,1,prices,dp,n);
-            return dp[i][j]=max(take,nottake);
-        }
-        else{
-              int take=prices[i]+maximum(i+1,1,prices,dp,n);
-            int nottake=maximum(i+1,0,prices,dp,n);
-             return dp[i][j]=max(take,nottake);
-        }
-    }
+    // int maximum(int i,int j,vector<int>&prices,vector<vector<int>>&dp,int n){
+    //     if(i==n){
+    //         return 0;
+    //     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(j==1){
+    //        int take=-prices[i]+maximum(i+1,0,prices,dp,n);
+    //         int nottake=maximum(i+1,1,prices,dp,n);
+    //         return dp[i][j]=max(take,nottake);
+    //     }
+    //     else{
+    //           int take=prices[i]+maximum(i+1,1,prices,dp,n);
+    //         int nottake=maximum(i+1,0,prices,dp,n);
+    //          return dp[i][j]=max(take,nottake);
+    //     }
+    // }
 public:
-    int maxProfit(vector<int>& prices) {
-        int n=prices.size();
-        vector<vector<int>>dp(n,vector<int>(2,-1));
-        return maximum(0,1,prices,dp,n);
+    int maxProfit(vector<int>& a) {
+        // int n=prices.size();
+        // vector<vector<int>>dp(n,vector<int>(2,-1));
+        // return maximum(0,1,prices,dp,n);
+        
+// @dhairyarajbabbar7584
+// 2 years ago
+// my approach of this question
+
+        int curr=a[0];
+        int profit=0;
+        for(int i=1;i<a.size();i++){
+            if(a[i]<curr) curr=a[i];
+            else if(a[i]>curr) {
+                profit+=a[i]-curr;
+                curr=a[i];
+            }
+        }
+        return profit;
     }
+    
 };
