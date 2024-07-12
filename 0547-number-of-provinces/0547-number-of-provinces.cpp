f4f19bc8de581854414e37b0ea57cc5db4233b5a
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-12 13:29:30 +0530

    Time: 20 ms (67.29%), Space: 19.8 MB (16.42%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,32 +1,34 @@
 class Solution {
-     void dfs(int start,vector<int>&vis,vector<int>adj[]){
-         vis[start]=1;
-         for(auto it:adj[start]){
-             if(!vis[it]){
-                 dfs(it,vis,adj);
-             }
-         }
-     }
+    void dfs(int start,vector<int>list[],vector<int>&vis){
+        vis[start]=1;
+        for(auto it:list[start]){
+            if(!vis[it]){
+                dfs(it,list,vis);
+            }
+        }
+    }
 public:
     int findCircleNum(vector<vector<int>>& adj) {
-        int v=adj.size();
-        vector<int>list[v];
-       for(int i=0;i<v;i++){
-         for(int j=0;j<v;j++){
-             if(adj[i][j]==1&&i!=j){
-                 list[i].push_back(j);
-                  list[j].push_back(i);
-             }
-         }
-       }
-        vector<int>vis(v,0);
+        int n=adj.size();
+        int m=adj[0].size();
+      vector<int>list[n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i!=j &&adj[i][j]==1){
+                  list[i].push_back(j);
+                    list[j].push_back(i);
+                }
+            }
+        }
+        vector<int>vis(n,0);
         int count=0;
-        for(int i=0;i<v;i++){
+        for(int i=0;i<n;i++){
             if(!vis[i]){
-               count++;
-                dfs(i,vis,list);
+                dfs(i,list,vis);
+                count++;
             }
         }
         return count;
+        
     }
 };
