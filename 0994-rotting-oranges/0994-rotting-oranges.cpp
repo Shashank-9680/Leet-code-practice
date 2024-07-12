1dbfcb0b352869ad24734d31e0d4723c8aa15d94
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-12 13:55:54 +0530

    Time: 7 ms (42.40%), Space: 16.6 MB (11.81%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -2,52 +2,53 @@ class Solution {
    
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int n = grid.size();
+         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<vector<int>>vis(n,vector<int>(m,0));
         queue<pair<int, int>> q;
 
-        // Add all initially rotten oranges to the queue
+        // Push all initially rotten oranges into the queue
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 2) {
                     q.push({i, j});
-                    vis[i][j] = 1;
+                    vis[i][j]=1;
                 }
             }
         }
 
+        // Directions array to traverse up, left, down, right
         vector<pair<int, int>> dir = {{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         int time = 0;
 
+        // BFS to process all rotten oranges level by level (minute by minute)
         while (!q.empty()) {
             int size = q.size();
-            bool hasRotten = false;
-
+            bool isRotten = false;
             for (int i = 0; i < size; i++) {
-                pair<int, int> it = q.front();
+                auto it = q.front();
+                q.pop();
                 int row = it.first;
                 int col = it.second;
-                q.pop();
 
                 for (auto d : dir) {
                     int nrow = row + d.first;
                     int ncol = col + d.second;
-
-                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1) {
+                    // If adjacent cell is a fresh orange, rot it and push it into the queue
+                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == 1&&!vis[nrow][ncol]) {
                         grid[nrow][ncol] = 2;
-                        vis[nrow][ncol] = 1;
                         q.push({nrow, ncol});
-                        hasRotten = true;
+                        isRotten = true;
+                        vis[nrow][ncol]=1;
                     }
                 }
             }
-
-            if (hasRotten) {
+            if (isRotten) {
                 time++;
             }
         }
 
+        // Check if there are any fresh oranges left
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (grid[i][j] == 1) {
@@ -55,7 +56,7 @@ public:
                 }
             }
         }
-        
+
         return time;
     }
 };
