c9890f1d7971c43f2a639b898ba93d15e4a5d131
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-13 13:06:56 +0530

    Time: 0 ms (100.00%), Space: 17.7 MB (6.41%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-          vector<vector<int>>result=image;
-        int n=image.size();
-        int m=image[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
+    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int intial=grid[sr][sc];
         queue<pair<int,int>>q;
         q.push({sr,sc});
-        int initialColor = result[sr][sc];
-        result[sr][sc]=color;
+        vector<vector<int>>vis(n,vector<int>(m,0));
         vis[sr][sc]=1;
-        vector<pair<int,int>>d={{-1,0},{0,-1},{0,1},{1,0}};
-        
+        grid[sr][sc]=color;
+        vector<pair<int,int>>dir={{-1,0},{0,-1},{0,1},{1,0}};
         while(!q.empty()){
             auto it=q.front();
             int row=it.first;
             int col=it.second;
-             q.pop();
-            // result[row][col]=color;
-            for(auto it:d){
+            q.pop();
+            for(auto it:dir){
                 int nrow=row+it.first;
                 int ncol=col+it.second;
                 if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]){
-                    if(result[nrow][ncol]==initialColor){
-                   
-                        result[nrow][ncol]=color;
-                        vis[nrow][ncol]=1;
+                    if(grid[nrow][ncol]==intial){
                         q.push({nrow,ncol});
+                        grid[nrow][ncol]=color;
+                        vis[nrow][ncol]=1;
                     }
                 }
-                
             }
-           
+            
         }
-        return result;
+        return grid;
        }
     
     
