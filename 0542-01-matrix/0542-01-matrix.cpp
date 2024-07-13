50325a498e101ed8f8f2927dac8abfd643c71b89
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-13 13:53:33 +0530

    Time: 41 ms (95.75%), Space: 38.2 MB (15.05%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,40 +1,41 @@
 class Solution {
+   
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-         int n=mat.size();
+        int n=mat.size();
         int m=mat[0].size();
+      vector<vector<int>>ans(n,vector<int>(m,0));
         vector<vector<int>>vis(n,vector<int>(m,0));
-        vector<vector<int>>dis(n,vector<int>(m));
-        // queue<pair<int,int>>q;
-        queue<pair<pair<int, int>, int>> q;
-        for(int i=0;i<n;i++ ){
+        queue<pair<pair<int,int>,int>>q;
+      
+        for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
+                    q.push({{i,j},1});
                     vis[i][j]=1;
-                    q.push({{i,j},0});      
-                } 
-            }
-        }
-           vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        while(!q.empty()){
-           
-              int row=q.front().first.first;
-            int col=q.front().first.second;
-            // int t=q.front().second;
-            int distance=q.front().second;
-            q.pop();
-            dis[row][col]=distance;
-            
-                   for (auto dir : directions) {
-                int newRow = row + dir.first;
-                int newCol = col + dir.second;
-                
-                if (newRow >= 0 && newRow < n && newCol >= 0 && newCol < m && vis[newRow][newCol] == 0) {
-                    vis[newRow][newCol] = 1;
-                    q.push({{newRow, newCol}, distance + 1});
                 }
             }
         }
-        return dis;
+        vector<pair<int,int>>dir={{-1,0},{0,-1},{1,0},{0,1}};
+        while(!q.empty()){
+            auto it=q.front();
+            int row=it.first.first;
+            int col=it.first.second;
+            int dis=it.second;
+            q.pop();
+            for(auto it:dir){
+                int nrow=row+it.first;
+                int ncol=col+it.second;
+                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]){
+                    if(mat[nrow][ncol]==1){
+                        ans[nrow][ncol]=dis;
+                        q.push({{nrow,ncol},dis+1});
+                        vis[nrow][ncol]=1;
+                    }
+                }
+            }
+            
+        }
+        return ans;
     }
 };
