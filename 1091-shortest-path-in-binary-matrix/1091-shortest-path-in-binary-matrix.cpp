83b0c998d2a39596fbbffacd0d38140e93a06278
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-22 13:13:53 +0530

    Time: 38 ms (89.77%), Space: 25.4 MB (23.86%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,33 +2,37 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>v(n,vector<int>(m,INT_MAX));
+       if(n==1&&grid[0][0]==0){
+           return 1;
+       }
+        vector<vector<int>>dist(n,vector<int>(n,INT_MAX));
         queue<pair<int,pair<int,int>>>q;
         q.push({0,{0,0}});
-        v[0][0]=0;
-            if (grid[0][0] != 0 || grid[n-1][m-1] != 0) return -1;
+        if(grid[0][0]!=0||grid[n-1][n-1]!=0) return -1;
+        dist[0][0]=1;
         while(!q.empty()){
-            int weight=q.front().first;
-            int node1=q.front().second.first;
-            int node2=q.front().second.second;
+            int dis=q.front().first;
+            int row=q.front().second.first;
+            int col=q.front().second.second;
             q.pop();
-            
-       for(int nr=-1;nr<=1;nr++){
-           for(int nc=-1;nc<=1;nc++){
-               int nrow=node1+nr;
-               int ncol=node2+nc;
-               if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&grid[nrow][ncol]==0){
-                   if(1+weight<v[nrow][ncol]){
-                       v[nrow][ncol]=1+weight;
-                       q.push({v[nrow][ncol],{nrow,ncol}});
-                   }
-               }
-           }
-       
-       }     
+            for(int rc=-1;rc<=1;rc++){
+                for(int cc=-1;cc<=1;cc++){
+                    int nrow=row+rc;
+                    int ncol=col+cc;  
+                    if(nrow>=0&&nrow<n&&ncol>=0&&ncol<n){
+                       if(grid[nrow][ncol]==0){
+                           if(dis+1<dist[nrow][ncol]){
+                               dist[nrow][ncol]=dis+1;
+                               q.push({dist[nrow][ncol],{nrow,ncol}});
+                           }
+                       } 
+                    }
+                }     
+            }
         }
-         return (v[n-1][m-1] == INT_MAX) ? -1 : v[n-1][m-1]+1;
-       
+       if(dist[n-1][n-1]==INT_MAX){
+           return -1;
+       }
+        return dist[n-1][n-1]+1;
     }
 };
