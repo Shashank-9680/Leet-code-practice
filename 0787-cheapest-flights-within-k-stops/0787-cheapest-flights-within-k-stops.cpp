acd8ccf6a4d5236afd84dc391f670def494b9827
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-22 18:12:54 +0530

    Time: 11 ms (92.94%), Space: 17.3 MB (25.59%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int>dist(n,INT_MAX);
+        queue<pair<int,pair<int,int>>>q;
+        q.push({0,{src,0}});
+        dist[src]=0;
         vector<pair<int,int>>adj[n];
         for(auto it:flights){
             adj[it[0]].push_back({it[1],it[2]});
         }
-        queue<pair<int,pair<int,int>>>q;
-        q.push({0,{src,0}});
-        vector<int>dist(n,INT_MAX);
-        dist[src]=0;
         while(!q.empty()){
-            int stop=q.front().first;
+         int stops=q.front().first;
             int node=q.front().second.first;
             int dis=q.front().second.second;
-           
             q.pop();
-             if(node==dst) continue;
-            if(stop>k) continue;
+            if(node==dst) continue;
+            if(stops>k) continue;
             for(auto it:adj[node]){
-                int adjnodes=it.first;
-                // if(adjnodes==dst){
-                //     continue;
-                // }
-                if(dis+it.second<dist[adjnodes]){
-                    dist[adjnodes]=dis+it.second;
-                    q.push({stop+1,{adjnodes,dis+it.second}});
+                if(dis+it.second<dist[it.first]){
+                    dist[it.first]=dis+it.second;
+                    q.push({stops+1,{it.first,dist[it.first]}});
                 }
             }
         }
-        if(dist[dst]==INT_MAX) return -1;
+        if(dist[dst]==INT_MAX){
+            return -1;
+        }
         return dist[dst];
     }
 };
