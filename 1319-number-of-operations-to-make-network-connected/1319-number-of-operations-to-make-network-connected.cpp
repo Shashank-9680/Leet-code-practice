25f64deda1ca6597a48b240d7bcff34e663bb8db
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-24 18:03:07 +0530

    Time: 77 ms (77.17%), Space: 47.1 MB (30.75%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,79 +1,80 @@
-class Solution {
-    class DisjointSet {
-    vector<int> rank, parent, size;
-public:
-    DisjointSet(int n) {
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1);
-        for (int i = 0; i <= n; i++) {
-            parent[i] = i;
-            size[i] = 1;
+class disjointSet{   
+    public:
+     vector<int>rank,parent,size;
+    disjointSet(int n){
+        rank.resize(n+1,0);
+         parent.resize(n+1,0);
+        size.resize(n+1,1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
         }
     }
-
-    int findUPar(int node) {
-        if (node == parent[node])
+    int findParent(int node){
+        if(parent[node]==node){
             return node;
-        return parent[node] = findUPar(parent[node]);
+        }
+        return node=findParent(parent[node]);
     }
-
-    void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank[ulp_u] < rank[ulp_v]) {
-            parent[ulp_u] = ulp_v;
+    void unionByRank(int u,int v){
+        int ultp_u=findParent(u);
+        int ultp_v=findParent(v);
+        if(ultp_u==ultp_v) return;
+        if(rank[ultp_u]==rank[ultp_v]){
+            parent[ultp_v]=ultp_u;
+            rank[ultp_u]++;
         }
-        else if (rank[ulp_v] < rank[ulp_u]) {
-            parent[ulp_v] = ulp_u;
+        else if(rank[ultp_u]>rank[ultp_v]){
+            parent[ultp_v]=ultp_u;
         }
-        else {
-            parent[ulp_v] = ulp_u;
-            rank[ulp_u]++;
-        }
-    }
-
-    void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size[ulp_u] < size[ulp_v]) {
-            parent[ulp_u] = ulp_v;
-            size[ulp_v] += size[ulp_u];
-        }
-        else {
-            parent[ulp_v] = ulp_u;
-            size[ulp_u] += size[ulp_v];
+        else{
+             parent[ultp_u]=ultp_v;
         }
     }
+     void unionBySize(int u,int v){
+        int ultp_u=findParent(u);
+        int ultp_v=findParent(v);
+        if(ultp_u==ultp_v) return;
+        if(size[ultp_u]==size[ultp_v]){
+            parent[ultp_v]=ultp_u;
+            size[ultp_u]=size[ultp_u]+size[ultp_v];
+        }
+        else if(size[ultp_u]>size[ultp_v]){
+            parent[ultp_v]=ultp_u;
+            size[ultp_u]=size[ultp_u]+size[ultp_v];
+        }
+        else{
+             parent[ultp_u]=ultp_v;
+              size[ultp_v]=size[ultp_v]+size[ultp_u];
+        }
+    }
+    
 };
+class Solution {
 public:
     int makeConnected(int n, vector<vector<int>>& connections) {
-        DisjointSet st(n);
-        int countxedges=0;
+        disjointSet ds(n);
+        int count=0;
         for(auto it:connections){
-            int i=it[0];
-            int j=it[1];
-            if(st.findUPar(i)==st.findUPar(j)){
-                countxedges++;
+            int u=it[0];
+            int v=it[1];
+            if(ds.findParent(u)!=ds.findParent(v)){
+               ds.unionBySize(u,v);
             }
             else{
-                st.unionBySize(i,j);
-            }
-        }
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(st.findUPar(i)==i){
                 count++;
             }
         }
-        int ans=count-1;
-        if(ans<=countxedges){
-            return ans;
+        int count1=0;
+        vector<int>arr=ds.parent;
+        for(int i=0;i<n;i++){
+            if(arr[i]==i){
+                count1++;
+            }
         }
-        else{
-            return -1;
+        int count2=count1-1;
+        if(count>=count2){
+            return count2;
         }
+        return -1;
     }
 };
