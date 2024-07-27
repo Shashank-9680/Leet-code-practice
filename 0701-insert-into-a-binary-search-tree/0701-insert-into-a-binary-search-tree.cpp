d5bc0139e5fa0ef8046d8f7c3c46cd3c7da36707
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-27 17:37:51 +0530

    Time: 63 ms (57.67%), Space: 57.5 MB (14.86%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -12,38 +12,62 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-       
-      TreeNode*temp=root;
+       TreeNode*curr=root;
         if(root==NULL){
-            TreeNode*newNode=new TreeNode(val);
-            return newNode;
+            return new TreeNode(val);
         }
-        TreeNode* temp1=NULL;
-        while(temp!=NULL){
-            if(temp->val>val){
-                temp=temp->left;
+//       TreeNode*temp=root;
+//         if(root==NULL){
+//             TreeNode*newNode=new TreeNode(val);
+//             return newNode;
+//         }
+//         TreeNode* temp1=NULL;
+//         while(temp!=NULL){
+//             if(temp->val>val){
+//                 temp=temp->left;
+//             }
+//             else{
+//                  temp1=temp;
+//                  temp=temp->right;
+//             }
+//         }
+        
+//         TreeNode*temp2=NULL;
+//         TreeNode*newNode=new TreeNode(val);
+//         if(temp1==NULL){
+//          TreeNode*newNode=new TreeNode(val);
+//             newNode->right=root;
+//             return newNode;
+//         }
+//         if(temp1->right==NULL){
+//             temp1->right=newNode;
+//             return root;
+//         }
+//         temp2=temp1->right;
+//         temp1->right=newNode;
+//         newNode->right=temp2;
+//         return root;
+        while(true){
+            if(root->val<val){
+                if(root->right!=NULL){
+                    root=root->right;
+                }
+                else{
+                    root->right=new TreeNode(val);
+                    break;
+                }
             }
             else{
-                 temp1=temp;
-                 temp=temp->right;
+                if(root->left!=NULL){
+                    root=root->left;
+                }
+                else{
+                    root->left=new TreeNode(val);
+                    break;
+                }
             }
+            
         }
-        
-        TreeNode*temp2=NULL;
-        TreeNode*newNode=new TreeNode(val);
-        if(temp1==NULL){
-         TreeNode*newNode=new TreeNode(val);
-            newNode->right=root;
-            return newNode;
-        }
-        if(temp1->right==NULL){
-            temp1->right=newNode;
-            return root;
-        }
-        temp2=temp1->right;
-        temp1->right=newNode;
-        newNode->right=temp2;
-        return root;
-        
+        return curr;
     }
 };
