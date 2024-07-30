c7824cc9ea91d91dfc839b3d7ee502f6b5275075
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-07-30 20:17:53 +0530

    Time: 3 ms (66.29%), Space: 20.2 MB (6.14%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.cpp
@@ -10,20 +10,20 @@
  * };
  */
 class Solution {
-    TreeNode*treemaking(int max,int &i,vector<int>&A){
-        if(i==A.size()||A[i]>max){
-            return NULL;
-        }
-        TreeNode*newNode=new TreeNode(A[i++]);
-        newNode->left=treemaking(newNode->val,i,A);
-        newNode->right=treemaking(max,i,A);
-        return newNode;
-        
-    }
+   
+   TreeNode*fxn(int ub,vector<int>v,int &i){
+       if(i==v.size()||v[i]>ub){
+           return NULL;
+       }
+       TreeNode*root=new TreeNode(v[i++]);
+       root->left=fxn(root->val,v,i);
+       root->right=fxn(ub,v,i);
+       return root;
+   }
 public:
     TreeNode* bstFromPreorder(vector<int>& preorder) {
         int i=0;
-       TreeNode*root= treemaking(INT_MAX,i,preorder);
-    return root;
-    }     
+    return fxn(INT_MAX,preorder,i);
+        
+    }
 };
