81fb560b17bb052228c342332abe8bf84fcce9aa
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-11 19:54:06 +0530

    Time: 22 ms (67.00%), Space: 22.1 MB (10.92%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -9,44 +9,16 @@
  */
 
 class Solution {
-//       bool pathNodes(TreeNode* root, TreeNode* p, vector<TreeNode*>& v) {
-//         if (!root) return false;
-//         v.push_back(root);
-//         if (root->val == p->val) return true;
-
-//         if (pathNodes(root->left, p, v) || pathNodes(root->right, p, v)) {
-//             return true;
-//         }
-//         v.pop_back();
-//         return false;
-//     }
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-//          vector<TreeNode*> arr1;
-//         vector<TreeNode*> arr2;
-
-//         pathNodes(root, p, arr1);
-//         pathNodes(root, q, arr2);
-
-//         TreeNode* lca = nullptr;
-//         for (int i = 0; i < min(arr1.size(), arr2.size()); i++) {
-//             if (arr1[i]->val == arr2[i]->val) {
-//                 lca = arr1[i];
-//             } else {
-//                 break;
-//             }
-//         }
-//         return lca;  
-//          if(root==NULL){
-//             return NULL;
-            
-//         }
-        int curr=root->val;
-        if(curr<p->val&&curr<q->val){
-            return lowestCommonAncestor(root->right,p,q);
+        if(root==NULL){
+            return NULL;
         }
-        if(curr>p->val&&curr>q->val){
-              return lowestCommonAncestor(root->left,p,q);
+        if(root->val>p->val &&root->val>q->val){
+            return lowestCommonAncestor(root->left,p,q);
+        }
+        if(root->val<p->val&&root->val<q->val){
+            return lowestCommonAncestor(root->right,p,q);
         }
         return root;
     }
