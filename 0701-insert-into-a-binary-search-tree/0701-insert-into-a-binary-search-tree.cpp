cd9722b9c6f65acc71abe536853a2e7ad2643132
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-11 18:48:43 +0530

    Time: 77 ms (6.52%), Space: 57.4 MB (38.54%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -12,43 +12,22 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-       TreeNode*curr=root;
+        TreeNode*temp=root;
         if(root==NULL){
             return new TreeNode(val);
         }
-//       TreeNode*temp=root;
-//         if(root==NULL){
-//             TreeNode*newNode=new TreeNode(val);
-//             return newNode;
-//         }
-//         TreeNode* temp1=NULL;
-//         while(temp!=NULL){
-//             if(temp->val>val){
-//                 temp=temp->left;
-//             }
-//             else{
-//                  temp1=temp;
-//                  temp=temp->right;
-//             }
-//         }
-        
-//         TreeNode*temp2=NULL;
-//         TreeNode*newNode=new TreeNode(val);
-//         if(temp1==NULL){
-//          TreeNode*newNode=new TreeNode(val);
-//             newNode->right=root;
-//             return newNode;
-//         }
-//         if(temp1->right==NULL){
-//             temp1->right=newNode;
-//             return root;
-//         }
-//         temp2=temp1->right;
-//         temp1->right=newNode;
-//         newNode->right=temp2;
-//         return root;
         while(true){
-            if(root->val<val){
+            if(root->val>val){
+                if(root->left!=NULL){
+                    root=root->left;
+                }
+                else{
+                    
+                    root->left=new TreeNode(val);
+                    break;
+                }
+            }
+            else{
                 if(root->right!=NULL){
                     root=root->right;
                 }
@@ -57,17 +36,7 @@ public:
                     break;
                 }
             }
-            else{
-                if(root->left!=NULL){
-                    root=root->left;
-                }
-                else{
-                    root->left=new TreeNode(val);
-                    break;
-                }
-            }
-            
         }
-        return curr;
+        return temp;
     }
 };
