5637a87a5829b581ec0d25e5c9f62dc9425eb457
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-12 23:22:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sum of Middle elements of two sorted arrays/sum-of-middle-elements-of-two-sorted-arrays.cpp
@@ -0,0 +1,71 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+//Position this line where user code will be pasted.
+class Solution {
+  public:
+    int SumofMiddleElements(vector<int> &arr1, vector<int> &arr2) {
+        int n=arr1.size();
+       int low=0;
+       int high=n;
+     int left=n;
+     
+       while(low<=high){
+           int mid1=(low+high)>>1;
+           int mid2=left-mid1;
+           int l1=INT_MIN;
+           int l2=INT_MIN;
+           int r1=INT_MAX;
+           int r2=INT_MAX;
+           if(mid1<n)r1=arr1[mid1];
+           if(mid2<n)r2=arr2[mid2];
+           if(mid1-1>=0)l1=arr1[mid1-1];
+           if(mid2-1>-0)l2=arr2[mid2-1];
+           if(l1<=r2&&l2<=r1){
+               return (max(l1,l2)+min(r1,r2));
+           }
+           else if(l1>r2){
+               high=mid1-1;
+           }
+           else{
+               low=mid1+1;
+           }
+       }
+       return 0;
+    }
+};
+
+//{ Driver Code Starts.
+//Back-end complete function template in C++
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr, brr;
+        string input1;
+        getline(cin, input1);
+        stringstream ss1(input1);
+        int number1;
+        while (ss1 >> number1) {
+            arr.push_back(number1);
+        }
+        string input2;
+        getline(cin, input2);
+        stringstream ss2(input2);
+        int number2;
+        while (ss2 >> number2) {
+            brr.push_back(number2);
+        }
+        Solution ob;
+        int res = ob.SumofMiddleElements(arr, brr);
+        cout << res << endl;
+    }
+}
+// } Driver Code Ends
