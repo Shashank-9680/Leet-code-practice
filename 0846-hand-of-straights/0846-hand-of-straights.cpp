80b80c1ca567d233c09561d6bb2a567ad2957476
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-13 17:30:29 +0530

    Time: 42 ms (87.29%), Space: 30.9 MB (53.21%) - LeetHub

--- 0846-hand-of-straights/0846-hand-of-straights.cpp
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -1,45 +1,30 @@
 class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
-        map<int,int>m;
-        if(hand.size()%groupSize!=0){
+        int n = hand.size();
+        if (n % groupSize != 0) {
             return false;
         }
-        for(auto &value:hand){
-            m[value]++;
+
+        sort(hand.begin(), hand.end());
+        unordered_map<int, int> m;
+        for (auto it : hand) {
+            m[it]++;
         }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-    for(auto values:m){
-        pq.push({values.first,values.second});
-        
-    }
-   
-     while (!pq.empty()) {
-            auto it = pq.top();
-            pq.pop();
 
-           
-            if (m[it.first] == 0) continue;
-
-            int count = m[it.first];
-
-          
-            for (int i = 0; i < groupSize; ++i) {
-                int card = it.first + i;
-                if (m[card] < count) {
-                    return false; 
-                }
-                m[card] -= count;
-
-               
-                if (m[card] > 0) {
-                    pq.push({card, m[card]});
+        for (int i = 0; i < n; i++) {
+            if (m[hand[i]] > 0) {
+                // Try to form a group starting from hand[i]
+                for (int j = 0; j < groupSize; j++) {
+                    int current = hand[i] + j;
+                    if (m[current] > 0) {
+                        m[current]--;  // Use one card from the current group
+                    } else {
+                        return false;  // Can't form a group starting from hand[i]
+                    }
                 }
             }
         }
-
-        return true; 
-      
+        return true;
     }
-    
 };
