a7326f4e771fc556eb95c0c0e0744fe61a9d8401
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-15 18:50:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Fruit Into Baskets/fruit-into-baskets.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int totalFruits(vector<int> &arr) {
+        int i=0;
+        int j=0;
+        int k=2;
+        int n=arr.size();
+        unordered_map<int,int>m;
+        int count=0;
+        int maxFruit=0;
+        while(j<n){
+            m[arr[j]]++;
+            while(m.size()>k){
+                m[arr[i]]--;
+                if(m[arr[i]]==0){
+                    m.erase(arr[i]);
+                }
+                i++;
+                
+            }
+            maxFruit=max(j-i+1,maxFruit);
+            j++;
+        }
+        return maxFruit;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // To ignore any newline character left in the buffer
+    while (t--) {
+        string line;
+        getline(cin, line); // Read the entire line of input
+        stringstream ss(line);
+        vector<int> arr;
+        int num;
+
+        // Parse the input line into integers and add to the vector
+        while (ss >> num) {
+            arr.push_back(num);
+        }
+        Solution ob;
+        int ans = ob.totalFruits(arr);
+
+        cout << ans << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
