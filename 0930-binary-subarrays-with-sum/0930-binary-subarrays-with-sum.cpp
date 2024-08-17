1b1caaaeb1df88a34619b1d900ab671de9bb802e
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-17 23:49:00 +0530

    Time: 12 ms (97.41%), Space: 31.4 MB (55.49%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,48 +1,27 @@
 class Solution {
-    int count(vector<int>nums,int goal){
-        int n = nums.size();
-        int i = 0;
-        int j = 0;
-        int sum = 0;
-        int count = 0;
-       if(goal<0){
-           return 0;
-       } 
-        while (j < n) {
-            sum=sum+ nums[j];
-            
-            while (sum > goal) {
-                sum =sum- nums[i];
+    int subArray1(vector<int>&nums,int goal){
+        if(goal<0){
+            return 0;
+        }
+        int i=0,j=0;
+        int count=0;
+        int sum=0;
+        while(j<nums.size()){
+            sum=sum+nums[j];
+            while(sum>goal){
+                sum=sum-nums[i];
                 i++;
             }
-            
-            count =count+ (j - i + 1);
+            count+=j-i+1;
             j++;
         }
-        
         return count;
     }
+    
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-//         unordered_map<int, int> prefixSumCount;
-//         int currentSum = 0;
-//         int count = 0;
-
-//         prefixSumCount[0] = 1;  
-
-//         for (int num : nums) {
-//             currentSum += num;
-//             if (prefixSumCount.find(currentSum - goal) != prefixSumCount.end()) {
-//                 count += prefixSumCount[currentSum - goal];
-//             }
-//             prefixSumCount[currentSum]++;
-//         }
-
-//         return count;
-//         }
-        
-    int count1=count(nums,goal);
-        int count2=count(nums,goal-1);
-        return count1-count2;
+        int count1=subArray1(nums,goal);
+        int count2=subArray1(nums,goal-1);
+        return abs(count1-count2);
     }
 };
