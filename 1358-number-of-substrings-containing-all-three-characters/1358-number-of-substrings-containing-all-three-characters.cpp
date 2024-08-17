56e8c6fcaf0f50c1de885fa39383570441b33b8a
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-18 01:39:45 +0530

    Time: 87 ms (5.21%), Space: 10 MB (41.62%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -6,12 +6,15 @@ public:
        int count=0;
         while(i<s.size()){
              arr[s[i]-'a']=i;
-                i++;
+              
             if(arr[0]>-1&&arr[1]>-1&&arr[2]>-1){
                int minimum1=min(arr[0],arr[1]);
                 int minimum=min(minimum1,arr[2]);
+                // cout<<minimum;
+                cout<<count;
                 count=count+minimum+1;   
             }
+              i++;
             
                
         }
