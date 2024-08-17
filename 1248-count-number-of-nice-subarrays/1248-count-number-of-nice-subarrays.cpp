3566f6463f5bc698fe24db9178539375d987c282
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-17 23:53:42 +0530

    Time: 83 ms (87.61%), Space: 70.3 MB (60.02%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,29 +1,28 @@
 class Solution {
-    int count(vector<int>nums,int k){
-          int i=0;
-        int j=0;
-        int oddcount=0;
+    int subArray1(vector<int>&nums,int goal){
+        
+        int i=0,j=0;
         int count=0;
-        int n=nums.size();
-       
-        while(j<n){
-          if(nums[j]%2!=0){
-              oddcount++;
-          }
-        while(oddcount>k){
-                if(nums[i]%2!=0) oddcount--;
+        int oddCount=0;
+        while(j<nums.size()){
+           if(nums[j]%2==1){
+               oddCount++;
+           }
+            while(oddCount>goal){
+                if(nums[i]%2==1){
+                    oddCount--;
+                }
                 i++;
             }
-            count=count+j-i+1;
+            count+=j-i+1;
             j++;
         }
-            return count;
+        return count;
     }
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-     int count1=count(nums,k);
-        int count2=count(nums,k-1);
-        return count1-count2;
-        
+        int count1=subArray1(nums,k);
+        int count2=subArray1(nums,k-1);
+        return abs(count1-count2);
     }
 };
