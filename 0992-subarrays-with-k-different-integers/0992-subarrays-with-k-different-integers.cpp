478766b09c4a7a6b850a9108fc7e52288daa1206
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-20 00:44:00 +0530

    Time: 127 ms (55.70%), Space: 56.3 MB (37.52%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.cpp
@@ -1,28 +1,31 @@
 class Solution {
-    int count(vector<int>nums,int k){
-        int count=0;
-        map<int,int>m;
-        int n=nums.size();
-        int i=0;
-        int j=0;
-        while(j<n){
-            m[nums[j]]++;
-            while(m.size()>k){
-                m[nums[i]]--;
-                if(m[nums[i]]==0){
-                    m.erase(nums[i]);      
-                }
-                i++;
-            }
-           count=count+j-i+1;
-            j++;
+    int subArray1(vector<int>&nums,int k){
+      unordered_map<int,int>m;
+   int i=0;
+   int j=0;
+   int n=nums.size();
+   int count=0;
+   while(j<n){
+     m[nums[j]]++;
+     while(m.size()>k){
+        m[nums[i]]--;
+        if(m[nums[i]]==0){
+            m.erase(nums[i]);
         }
-        return count;
+       
+        i++;
+      }
+     count+=(j-i+1);
+      j++;
+     
+   }
+   return count;
     }
 public:
     int subarraysWithKDistinct(vector<int>& nums, int k) {
-    int count1=count(nums,k);
-        int count2=count(nums,k-1);
-        return count1-count2;
+        
+        int count1=subArray1(nums,k);
+        int count2=subArray1(nums,k-1);
+        return abs(count1-count2);
     }
 };
