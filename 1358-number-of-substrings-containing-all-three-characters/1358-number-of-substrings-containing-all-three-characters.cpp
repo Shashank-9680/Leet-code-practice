073ec8ce9ef87b98655c15f4c06eab3c878943d3
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-08-19 23:47:39 +0530

    Time: 50 ms (34.78%), Space: 10 MB (59.50%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int i=0;
      int arr[3]={-1,-1,-1};
-       int count=0;
-        while(i<s.size()){
-             arr[s[i]-'a']=i;
-              
-            if(arr[0]>-1&&arr[1]>-1&&arr[2]>-1){
-               int minimum1=min(arr[0],arr[1]);
-                int minimum=min(minimum1,arr[2]);
-                // cout<<minimum;
+        int i=0;
+        int n=s.size();
+        int count=0;
+        while(i<n){
+            arr[s[i]-'a']=i;
+            if(arr[0]>=0&&arr[1]>=0&&arr[2]>=0){
+                int index=min(arr[0],min(arr[1],arr[2]));
+                count+=index+1;
                 cout<<count;
-                count=count+minimum+1;   
             }
-              i++;
-            
-               
+            i++;
         }
         return count;
     }
