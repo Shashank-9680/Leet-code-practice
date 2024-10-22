1072642234e7ccc5276c3c5e7bef7d1a95a01d3e
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-10-22 13:14:55 +0530

    Time: 143 ms (68.33%), Space: 180.3 MB (5.08%) - LeetHub

--- 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
+++ 2583-kth-largest-sum-in-a-binary-tree/2583-kth-largest-sum-in-a-binary-tree.cpp
@@ -1,43 +1,54 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
     long long kthLargestLevelSum(TreeNode* root, int k) {
-        if (!root) return -1;
-        
-        queue<TreeNode*> q;
-        priority_queue<long long, vector<long long>, greater<long long>> pq;
-        q.push(root);
-
-        while (!q.empty()) {
-            int levelSize = q.size();
-            long long levelSum = 0;
-            
-            for (int i = 0; i < levelSize; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-                
-                levelSum += node->val;
-
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-
-            pq.push(levelSum);
-            if (pq.size() > k) {
-                pq.pop();  // Keep only the k largest sums in the priority queue
-            }
+        // if(k==1) return root->val;
+        queue<pair<TreeNode*,int>>q;
+        vector<pair<TreeNode*,int>>v;
+        vector<long long int>ans;
+     
+        q.push({root,0});
+       
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            TreeNode*temp=it.first;
+            int count=it.second;
+            v.push_back({temp,count});
+            if(temp->left!=NULL) q.push({temp->left,count+1});
+            if(temp->right!=NULL) q.push({temp->right,count+1});
         }
-
-        return pq.size() == k ? pq.top() : -1;  
+      int count=0;
+        long long int sum=v[0].first->val;
+      
+       for(int i=1;i<v.size();i++){
+           if(v[i].second==count){
+               sum=sum+v[i].first->val;
+           }
+           else{
+            ans.push_back(sum);
+               count++;
+             sum=v[i].first->val;
+           }
+       }
+        ans.push_back(sum);
+         
+       
+         if(k>ans.size()){
+            return -1;
+        }
+         for(auto it:ans){
+             cout<<it;
+         }
+       priority_queue<long long int,vector<long long int>,greater<long long int>>pq;
+       for(auto it:ans){ 
+               pq.push(it);
+            if(pq.size()>k){
+                pq.pop();
+            }
+       }  
+      
+    
+       
+        return pq.top();
     }
 };
