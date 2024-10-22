163866b6733c67c8893fe62af8748012cf1c1220
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2024-10-23 00:05:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find the Sum of Last N nodes of the Linked List/find-the-sum-of-last-n-nodes-of-the-linked-list.cpp
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+class Solution {
+    Node* reverse(Node*head){
+        Node*prev=NULL;
+        Node*temp=head;
+        while(temp!=NULL){
+            Node*curr=temp->next;
+            temp->next=prev;
+            prev=temp;
+            temp=curr;
+        }
+        return prev;
+    }
+  public:
+  
+    int sumOfLastN_Nodes(struct Node* head, int n) {
+      Node*newHead=reverse(head);
+      Node*temp=newHead;
+      int sum=0;
+      int count=1;
+      while(temp!=NULL){
+          sum=sum+temp->data;
+          if(count==n){
+              return sum;
+          }
+          count++;
+          temp=temp->next;
+      }
+      return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int n;
+        cin >> n;
+        cin.ignore();
+        struct Node *head = new Node(arr[0]);
+        struct Node *tail = head;
+        for (int i = 1; i < arr.size(); ++i) {
+            tail->next = new Node(arr[i]);
+            tail = tail->next;
+        }
+        Solution ob;
+        cout << ob.sumOfLastN_Nodes(head, n) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
