48fee09948a2e8a81fc51a62e5eb1d05b17e930d
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-03 10:07:02 +0530

    Time: 3 ms (13.41%), Space: 18.8 MB (8.75%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,36 +1,31 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& grid, int sr, int sc, int color) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int intial=grid[sr][sc];
-        queue<pair<int,int>>q;
-        q.push({sr,sc});
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int n=image.size();
+        int m = image[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>>ans=image;
+        queue<pair<int,int>>q;
+        ans[sr][sc]=color;
         vis[sr][sc]=1;
-        grid[sr][sc]=color;
-        vector<pair<int,int>>dir={{-1,0},{0,-1},{0,1},{1,0}};
+        q.push({sr,sc});
+        vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
         while(!q.empty()){
-            auto it=q.front();
-            int row=it.first;
-            int col=it.second;
-            q.pop();
-            for(auto it:dir){
-                int nrow=row+it.first;
-                int ncol=col+it.second;
-                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]){
-                    if(grid[nrow][ncol]==intial){
-                        q.push({nrow,ncol});
-                        grid[nrow][ncol]=color;
-                        vis[nrow][ncol]=1;
-                    }
+          int row=q.front().first;
+          int col =q.front().second;
+          q.pop();
+          for(auto it:dir){
+            int nrow= row+it.first;
+            int ncol =col+it.second;
+            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&& !vis[nrow][ncol]){
+                if(image[nrow][ncol]==image[sr][sc]){
+                    q.push({nrow,ncol});
+                    ans[nrow][ncol]=color;
+                    vis[nrow][ncol]=1;
                 }
             }
-            
+          }
         }
-        return grid;
-       }
-    
-    
-    
+        return ans;
+    }
 };
