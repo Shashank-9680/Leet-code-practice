44a0e893a511c736d1f1f65a563e57956d15d72c
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-04 09:03:56 +0530

    Time: 20 ms (50.34%), Space: 38.7 MB (41.86%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,13 +1,12 @@
 class Solution {
-   
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n=mat.size();
         int m=mat[0].size();
-      vector<vector<int>>ans(n,vector<int>(m,0));
-        vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<vector<int>>ans=mat;
         queue<pair<pair<int,int>,int>>q;
-      
+        vector<vector<int>>vis(n,vector<int>(m,0));
+         vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
@@ -16,26 +15,27 @@ public:
                 }
             }
         }
-        vector<pair<int,int>>dir={{-1,0},{0,-1},{1,0},{0,1}};
+
         while(!q.empty()){
-            auto it=q.front();
-            int row=it.first.first;
-            int col=it.first.second;
-            int dis=it.second;
-            q.pop();
-            for(auto it:dir){
-                int nrow=row+it.first;
-                int ncol=col+it.second;
-                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]){
-                    if(mat[nrow][ncol]==1){
-                        ans[nrow][ncol]=dis;
-                        q.push({{nrow,ncol},dis+1});
-                        vis[nrow][ncol]=1;
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int row=q.front().first.first;
+                int col=q.front().first.second;
+                int count =q.front().second;
+                q.pop();
+                for(auto it:dir){
+                    int nrow= row+it.first;
+                    int ncol =col+it.second;
+                    if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m){
+                        if(mat[nrow][ncol]==1&&!vis[nrow][ncol]){
+                            q.push({{nrow,ncol},count+1});
+                            ans[nrow][ncol]=count;
+                            vis[nrow][ncol]=1;
+                        }
                     }
                 }
             }
-            
         }
-        return ans;
+    return ans;
     }
 };
