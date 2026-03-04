5555decc621812aab081a2750c8ce7a18e4af217
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-04 09:47:34 +0530

    Time: 0 ms (100%), Space: 14.9 MB (30.79%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,49 +1,57 @@
 class Solution {
-    void dfs(int row,int col,vector<vector<char>>& board,vector<vector<int>>&vis){
-        int n=board.size();
-        int m=board[0].size();
-       vis[row][col]=1;
-        vector<pair<int,int>>dir={{-1,0},{0,-1},{1,0},{0,1}};
-        for(auto it:dir){
-            int nrow=row+it.first;
-            int ncol=col+it.second;
-            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m &&!vis[nrow][ncol]){
-                if(board[nrow][ncol]=='O'){
-                    dfs(nrow,ncol,board,vis);
-                }
-            }
-        }
-    }
-    
 public:
     void solve(vector<vector<char>>& board) {
-     int n=board.size();
-        int m=board[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        for(int j=0;j<m;j++){
-            if(!vis[0][j]&&board[0][j]=='O'){
-                dfs(0,j,board,vis);
-            }
-            if(!vis[n-1][j]&&board[n-1][j]=='O'){
-                dfs(n-1,j,board,vis);
-            }
-            
+       int n=board.size();
+       int m=board[0].size();
+       queue<pair<int,int>>q;
+     vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
+
+       vector<vector<int>>vis(n,vector<int>(m,0));
+       for(int i=0;i<m;i++){
+         if(board[0][i]=='O'){
+            q.push({0,i});
+            vis[0][i]=1;
+         }
+       }
+       for(int j=1;j<n;j++){
+        if(board[j][0]=='O'){
+            q.push({j,0});
+            vis[j][0]=1;
         }
-        for(int i=1;i<n-1;i++){
-            if(!vis[i][0]&&board[i][0]=='O'){
-                dfs(i,0,board,vis);
-            }
-            if(!vis[i][m-1]&&board[i][m-1]=='O'){
-                dfs(i,m-1,board,vis);
-            }
+       }
+       for(int i=1;i<m;i++){
+        if(board[n-1][i]=='O'){
+            q.push({n-1,i});
+            vis[n-1][i]=1;
         }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j]&&board[i][j]=='O'){
-                    board[i][j]='X';
+       }
+       for(int j=1;j<n-1;j++){
+        if(board[j][m-1]=='O'){
+            q.push({j,m-1});
+            vis[j][m-1]=1;
+        }
+       }
+       while(!q.empty()){
+        int row=q.front().first;
+        int col =q.front().second;
+        q.pop();
+        for(auto it:dir){
+            int nrow=it.first+row;
+            int ncol=it.second+col;
+            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]){
+                if(board[nrow][ncol]=='O'){
+                    vis[nrow][ncol]=1;
+                    q.push({nrow,ncol});
                 }
             }
         }
-      
+       }
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(board[i][j]=='O'&&vis[i][j]==0){
+                board[i][j]='X';
+            }
+        }
+       }
     }
 };
