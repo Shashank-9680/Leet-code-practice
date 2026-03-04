877ac4df7892e7e5415e7818edd42b3c6ae3fe75
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-04 10:35:05 +0530

    Time: 33 ms (21.17%), Space: 24.4 MB (16.98%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,42 +1,46 @@
 class Solution {
-    void dfs(vector<vector<int>>&vis,vector<vector<char>>& grid,int row,int col){
-          int n=grid.size();
-        int m=grid[0].size();
-        vis[row][col]=1;
-    // queue<pair<int,int>>q;
-    //     q.push({row,col});
-        // while(!q.empty()){
-        //     int currrow=q.front().first;
-        //     int currcol=q.front().second;
-        //     q.pop();
-             vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-          for (auto dir : directions) {
-                   int nrow=row+dir.first;
-                    int ncol=col+dir.second;
-                  if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && 
-                    !vis[nrow][ncol] && grid[nrow][ncol] == '1') {
-                    // vis[nrow][ncol] = 1;
-                    // q.push({nrow, ncol});
-                       dfs(vis,grid,nrow,ncol);   
-               }
-           } 
-        // }
-    }
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        vector<vector<int>>vis(n,vector<int>(m,0));
-        
-        int count=0;
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
+
+        int count = 0;
+
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(!vis[i][j]&&grid[i][j]=='1'){
+
+                if(grid[i][j]=='1' && !vis[i][j]){
+
                     count++;
-                    dfs(vis,grid,i,j);
+
+                    queue<pair<int,int>> q;
+                    q.push({i,j});
+                    vis[i][j]=1;
+
+                    while(!q.empty()){
+                        int row=q.front().first;
+                        int col=q.front().second;
+                        q.pop();
+
+                        for(auto it:dir){
+                            int nrow=row+it.first;
+                            int ncol=col+it.second;
+
+                            if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+                                if(grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
+                                    q.push({nrow,ncol});
+                                    vis[nrow][ncol]=1;
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
+
         return count;
     }
 };
