1ea9797cfb293185968f9ed7aabe13c9a7d5c463
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-05 08:11:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,48 +1,42 @@
 class Solution {
+    bool dfs(int node,vector<int>&vis,vector<int>adj[],int parent){
+        for(auto it:adj[node]){
+            if(!vis[it]){
+                vis[it]=1;
+                if(dfs(it,vis,adj,node)){
+                    return true;
+                }
+            }
+            else if(it!=parent){
+                return true;
+            }
+        }
+        return false;
+    }
 public:
     bool isCycle(int V, vector<vector<int>>& edges) {
 
-        vector<int> adj[V];
-
-        // build adjacency list
-        for(auto &e : edges){
-            int u = e[0];
-            int v = e[1];
+        vector<int>adj[V];
+        int e=edges.size();
+        for(int i=0;i<e;i++){
+            int u=edges[i][0];
+            int v=edges[i][1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
-        vector<int> vis(V,0);
-
+        
+        vector<int>vis(V,0);
+        int parent=-1;
         for(int i=0;i<V;i++){
-
             if(!vis[i]){
-
-                queue<pair<int,int>> q;
-                q.push({i,-1});
-                vis[i] = 1;
-
-                while(!q.empty()){
-
-                    int node = q.front().first;
-                    int parent = q.front().second;
-                    q.pop();
-
-                    for(auto it: adj[node]){
-
-                        if(!vis[it]){
-                            vis[it] = 1;
-                            q.push({it,node});
-                        }
-
-                        else if(parent != it){
-                            return true;
-                        }
-                    }
-                }
+                vis[i]=1;
+                 if(dfs(i,vis,adj,parent)){
+                
+                return true;
             }
+            }
+           
         }
-
         return false;
     }
 };
