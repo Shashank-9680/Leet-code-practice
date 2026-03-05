6fdaf1bf586e752bd59ab330af63e2c98ad144f6
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-05 07:50:01 +0530

    Time: 0 ms (100%), Space: 17.4 MB (55.95%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -1,33 +1,37 @@
 class Solution {
+    bool dfs(int node, vector<vector<int>>& graph, vector<int>& vis) {
+
+    
+    for(auto it:graph[node]){
+        if(vis[it]==-1){
+            vis[it]=!vis[node];
+            if(!dfs(it,graph,vis)){
+                return false;
+            }
+        }
+        else if(vis[it]==vis[node]){
+            return false;
+        }
+
+    }
+            
+      return true;          
+    }
+
 public:
     bool isBipartite(vector<vector<int>>& graph) {
-        int n=graph.size();
-        vector<int>vis(n,-1);
-        queue<int>q;
-     
-       for (int i = 0; i < n; i++) {
-            if (vis[i] == -1) { 
-                queue<int> q;
+        int n = graph.size();
+        vector<int> vis(n, -1);
+
+        for(int i = 0; i < n; i++){
+            if(vis[i] == -1){
                 vis[i] = 0;
-                q.push(i);
-
-                while (!q.empty()) {
-                    int node = q.front();
-                    q.pop();
-
-                    for (int adjNode : graph[node]) {
-                        if (vis[adjNode] != -1) {
-                            if (vis[adjNode] == vis[node]) {
-                                return false;
-                            }
-                        } else {
-                            vis[adjNode] = !vis[node];  // Assign the opposite color
-                            q.push(adjNode);
-                        }
-                    }
+                if(!dfs(i, graph, vis)){
+                    return false;
                 }
             }
         }
+
         return true;
     }
 };
