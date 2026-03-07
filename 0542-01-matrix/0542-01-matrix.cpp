7ac7fb9598933cb8ca9b0eb79afc4d2a4507a4a0
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-07 08:01:32 +0530

    Time: 19 ms (56.07%), Space: 38.7 MB (41.68%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -6,8 +6,8 @@ public:
         vector<vector<int>>ans=mat;
         queue<pair<pair<int,int>,int>>q;
         vector<vector<int>>vis(n,vector<int>(m,0));
-         vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
-        for(int i=0;i<n;i++){
+        vector<pair<int,int>>dir={{-1,0},{0,-1},{1,0},{0,1}};
+        for(auto i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
                     q.push({{i,j},1});
@@ -15,27 +15,23 @@ public:
                 }
             }
         }
-
         while(!q.empty()){
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                int row=q.front().first.first;
+           int row=q.front().first.first;
                 int col=q.front().first.second;
-                int count =q.front().second;
-                q.pop();
-                for(auto it:dir){
-                    int nrow= row+it.first;
-                    int ncol =col+it.second;
-                    if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m){
-                        if(mat[nrow][ncol]==1&&!vis[nrow][ncol]){
-                            q.push({{nrow,ncol},count+1});
-                            ans[nrow][ncol]=count;
+                int cnt=q.front().second;
+                 q.pop();
+                 for(auto it:dir){
+                    int nrow=row+it.first;
+                    int ncol=col+it.second;
+                    if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]){
+                        if(mat[nrow][ncol]==1){
+                            q.push({{nrow,ncol},cnt+1});
+                            ans[nrow][ncol]=cnt;
                             vis[nrow][ncol]=1;
                         }
                     }
-                }
-            }
+                 }
         }
-    return ans;
+        return ans;
     }
 };
