756867657e1e468729825c63087596e5dbf049e9
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-07 07:36:18 +0530

    Time: 2 ms (17.06%), Space: 18.7 MB (8.81%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -2,29 +2,29 @@ class Solution {
 public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int n=image.size();
-        int m = image[0].size();
+        int m=image[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
+        vector<pair<int,int>>dir={{-1,0},{0,-1},{1,0},{0,1}};
         vector<vector<int>>ans=image;
         queue<pair<int,int>>q;
-        ans[sr][sc]=color;
-        vis[sr][sc]=1;
         q.push({sr,sc});
-        vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}};
+        vis[sr][sc]=1;
+        ans[sr][sc]=color;
         while(!q.empty()){
-          int row=q.front().first;
-          int col =q.front().second;
-          q.pop();
-          for(auto it:dir){
-            int nrow= row+it.first;
-            int ncol =col+it.second;
-            if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&& !vis[nrow][ncol]){
-                if(image[nrow][ncol]==image[sr][sc]){
-                    q.push({nrow,ncol});
-                    ans[nrow][ncol]=color;
-                    vis[nrow][ncol]=1;
-                }
-            }
-          }
+            int row=q.front().first;
+                int col=q.front().second;
+                 q.pop();
+                 for(auto it:dir){
+                    int nrow=row+it.first;
+                    int ncol=col+it.second;
+                    if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]){
+                        if(image[nrow][ncol]==image[row][col]){
+                            q.push({nrow,ncol});
+                            ans[nrow][ncol]=color;
+                            vis[nrow][ncol]=1;
+                        }
+                    }
+                 }
         }
         return ans;
     }
