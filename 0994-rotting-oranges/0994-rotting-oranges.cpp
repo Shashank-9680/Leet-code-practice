5dfd8c223812b21409454e80cb080335f425c07f
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-07 07:31:47 +0530

    Time: 0 ms (100%), Space: 17.1 MB (30.58%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -5,44 +5,46 @@ public:
         int m=grid[0].size();
         vector<vector<int>>vis(n,vector<int>(m,0));
         queue<pair<int,int>>q;
-        vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
+        vector<pair<int,int>>dir={{-1,0},{0,-1},{1,0},{0,1}};
         int fresh=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]==2){
-                    vis[i][j]=1;
-                    q.push({i,j});
-                }
-                if(grid[i][j]==1){
-                    fresh++;
-                }
-            }
-        }
         int time=0;
+        for(int i=0;i<n;i++){
+          for(int j=0;j<m;j++){
+            if(grid[i][j]==2){
+                q.push({i,j});
+                vis[i][j]=1;
+            }
+            else if(grid[i][j]==1){
+                fresh++;
+            }
+          }
+        }
         while(!q.empty()){
             int size=q.size();
-            bool ifPresent=false;
-            for(int it=0;it<size;it++){
-               int row=q.front().first;
-               int col=q.front().second;
-               q.pop();
-               for(auto iter:dir){
-                int nrow=row+iter.first;
-                int ncol =col+iter.second;
-                if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]){
-                    if(grid[nrow][ncol]==1){
-                        grid[nrow][ncol]=2;
-                        fresh--;
-                        ifPresent=true;
-                        q.push({nrow,ncol});
+            bool isRotten=false;
+            for(int i=0;i<size;i++){
+                int row=q.front().first;
+                int col=q.front().second;
+                 q.pop();
+                 for(auto it:dir){
+                    int nrow=row+it.first;
+                    int ncol=col+it.second;
+                    if(nrow>=0&&nrow<n&&ncol>=0&&ncol<m&&!vis[nrow][ncol]){
+                        if(grid[nrow][ncol]==1){
+                            q.push({nrow,ncol});
+                            grid[nrow][ncol]=2;
+                            vis[nrow][ncol]=1;
+                            isRotten=true;
+                            fresh--;
+                        }
                     }
-                }
-                
-               }
+                 }
             }
-            if(ifPresent) time++;
-        } 
+            if(isRotten) time++;
+            
+
+        }
         if(fresh==0) return time;
         return -1;
-    }   
+    }
 };
