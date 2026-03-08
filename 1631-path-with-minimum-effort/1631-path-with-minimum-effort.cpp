2edab72d95a0cd9453c4afdb0351d24f9d3e66ed
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-08 09:30:08 +0530

    Time: 65 ms (38.42%), Space: 24.6 MB (56.42%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -1,42 +1,53 @@
 class Solution {
 public:
     int minimumEffortPath(vector<vector<int>>& heights) {
+
         int n = heights.size();
         int m = heights[0].size();
-        
-        // Vector to store the minimum effort to reach each cell
-        vector<vector<int>> v(n, vector<int>(m, INT_MAX));
-        v[0][0] = 0;
 
-        // Priority queue to store (effort, (row, col))
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> q;
-        q.push({0, {0, 0}});
-        
-        // Directions array to move in 4 directions
-        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        vector<vector<int>> dist(n, vector<int>(m, 1e9));
+        dist[0][0] = 0;
 
-        while (!q.empty()) {
-            int weight = q.top().first;
-            int row = q.top().second.first;
-            int col = q.top().second.second;
-            q.pop();
+        priority_queue<
+            pair<int,pair<int,int>>,
+            vector<pair<int,pair<int,int>>>,
+            greater<pair<int,pair<int,int>>>
+        > pq;
 
-           
+        pq.push({0,{0,0}});
 
-            for (auto it : directions) {
+        vector<pair<int,int>> neig = {
+            {-1,0},{1,0},{0,-1},{0,1}
+        };
+
+        while(!pq.empty()){
+
+            int dis = pq.top().first;
+            int row = pq.top().second.first;
+            int col = pq.top().second.second;
+            pq.pop();
+
+            for(auto it: neig){
+
                 int nrow = row + it.first;
                 int ncol = col + it.second;
-                
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m) {
-                    int effort = max(weight, abs(heights[nrow][ncol] - heights[row][col]));
-                    if (effort < v[nrow][ncol]) {
-                        v[nrow][ncol] = effort;
-                        q.push({effort, {nrow, ncol}});
+
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m){
+
+                    int diff = abs(heights[nrow][ncol] - heights[row][col]);
+
+                    int newEffort = max(dis, diff);
+
+                    if(newEffort < dist[nrow][ncol]){
+
+                        dist[nrow][ncol] = newEffort;
+
+                        pq.push({newEffort,{nrow,ncol}});
                     }
                 }
             }
         }
-        
-        return v[n-1][m-1];
+
+        return dist[n-1][m-1];
     }
 };
