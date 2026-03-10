b1fa2fbacae0c0468b85867e84f56ad3d275de08
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-10 09:20:48 +0530

    Time: 3 ms (72.46%), Space: 18.5 MB (53.13%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     vector<int> findOrder(int V, vector<vector<int>>& edges) {
-        vector<int>adj[V];
-        int e=edges.size();
-        for(int i=0;i<e;i++){
-            int u=edges[i][0];
-            int v=edges[i][1];
-            adj[u].push_back(v);
-        }
-        vector<int>inDegree(V,0);
-        queue<int>q;
-        for(int i = 0; i < V; i++){
-           for(auto it : adj[i]){
-              inDegree[it]++;
+          vector<int>adj[V];
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
         }
         
-}
-   for(int i=0;i<V;i++){
-       if(inDegree[i]==0){
-           q.push(i);
-       }
-   }
-   vector<int>ans;
-   while(!q.empty()){
-       int node=q.front();
-       q.pop();
-       ans.push_back(node);
-       for(auto it:adj[node]){
-         inDegree[it]--;
-         if(inDegree[it]==0){
-             q.push(it);
-         }
-       }
-   }
-     
-     reverse(ans.begin(),ans.end());
-     if(ans.size()!=V) return {};
-     return ans;
+        vector<int>inDegree(V,0);
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                inDegree[it]++;
+            }
+        }
+        
+        queue<int>q;
+        for(auto i=0;i<V;i++){
+            if(inDegree[i]==0){
+                q.push(i);
+            }
+        }
+        vector<int>ans;
+        while(!q.empty()){
+            int node=q.front();
+            q.pop();
+            ans.push_back(node);
+            for(auto it:adj[node]){
+                inDegree[it]--;
+                if(inDegree[it]==0){
+                    q.push(it);
+                }
+            }
+        }
+        if(ans.size()!=V){
+            return {};
+        }
+        reverse(ans.begin(),ans.end());
+         
+        return ans;
     }
 };
