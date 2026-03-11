957c55831d2c52f3edccfcaf944473c34fb12d26
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-11 08:57:31 +0530

    Time: 20 ms (49.22%), Space: 47.1 MB (34.65%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -1,80 +1,76 @@
-class disjointSet{   
+class DisjointSet{
     public:
-     vector<int>rank,parent,size;
-    disjointSet(int n){
+    vector<int>rank,parent,size;
+    DisjointSet(int n){
         rank.resize(n+1,0);
-         parent.resize(n+1,0);
+        parent.resize(n+1);
         size.resize(n+1,1);
         for(int i=0;i<=n;i++){
             parent[i]=i;
         }
     }
+    
     int findParent(int node){
-        if(parent[node]==node){
+        if(node==parent[node]){
             return node;
         }
-        return node=findParent(parent[node]);
+        return parent[node]=findParent(parent[node]);
     }
-    void unionByRank(int u,int v){
-        int ultp_u=findParent(u);
-        int ultp_v=findParent(v);
+    
+    void UnionByRank(int u,int v){
+        int ultp_u= findParent(u);
+        int ultp_v= findParent(v);
         if(ultp_u==ultp_v) return;
-        if(rank[ultp_u]==rank[ultp_v]){
-            parent[ultp_v]=ultp_u;
-            rank[ultp_u]++;
+        if(rank[ultp_u]<rank[ultp_v]){
+            parent[ultp_u]=ultp_v;
         }
         else if(rank[ultp_u]>rank[ultp_v]){
             parent[ultp_v]=ultp_u;
         }
         else{
-             parent[ultp_u]=ultp_v;
-        }
-    }
-     void unionBySize(int u,int v){
-        int ultp_u=findParent(u);
-        int ultp_v=findParent(v);
-        if(ultp_u==ultp_v) return;
-        if(size[ultp_u]==size[ultp_v]){
+            rank[ultp_u]++;
             parent[ultp_v]=ultp_u;
-            size[ultp_u]=size[ultp_u]+size[ultp_v];
-        }
-        else if(size[ultp_u]>size[ultp_v]){
-            parent[ultp_v]=ultp_u;
-            size[ultp_u]=size[ultp_u]+size[ultp_v];
-        }
-        else{
-             parent[ultp_u]=ultp_v;
-              size[ultp_v]=size[ultp_v]+size[ultp_u];
         }
     }
     
+    void UnionBySize(int u,int v){
+        int ultp_u= findParent(u);
+        int ultp_v= findParent(v);
+        if(ultp_u==ultp_v) return;
+        if(size[ultp_u]<size[ultp_v]){
+            parent[ultp_u]=ultp_v;
+            size[ultp_v]+=size[ultp_u];
+        }
+        else{
+            parent[ultp_v]=ultp_u;
+            size[ultp_u]+=size[ultp_v];
+        }
+    }
 };
+
 class Solution {
 public:
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        disjointSet ds(n);
+    int makeConnected(int V, vector<vector<int>>& connections) {
+        DisjointSet ds(V);
         int count=0;
+        int extraEdges=0;
         for(auto it:connections){
             int u=it[0];
             int v=it[1];
             if(ds.findParent(u)!=ds.findParent(v)){
-               ds.unionBySize(u,v);
+                ds.UnionBySize(u,v);
             }
             else{
-                count++;
+                extraEdges++;
             }
         }
-        int count1=0;
-        vector<int>arr=ds.parent;
-        for(int i=0;i<n;i++){
-            if(arr[i]==i){
-                count1++;
+        for(int i=0;i<V;i++){
+            if(ds.parent[i]==i){
+               count++;
             }
         }
-        int count2=count1-1;
-        if(count>=count2){
-            return count2;
-        }
+       int ans=count-1;
+        if(extraEdges>=ans) return ans;
         return -1;
     }
 };
