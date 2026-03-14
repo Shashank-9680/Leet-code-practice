316f07d9f6bdb5e1ad738b1fad7f69a326b388f9
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-14 10:29:45 +0530

    Time: 0 ms (100%), Space: 10.8 MB (66.24%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -10,62 +10,18 @@
  * };
  */
 class Solution {
-//       void inOrderTraversal(TreeNode*root,vector<int>&v){
-   
-//     if(root==nullptr){
-//         return;
-//     }
-   
-//     inOrderTraversal(root->left,v);
-//     v.push_back(root->val);
-//     inOrderTraversal(root->right,v);
-
-
-// }
 public:
+vector<int>ans;
+  void dfs(TreeNode* root){
+       if(root==NULL){
+        return;
+       }  
+       dfs(root->left);
+          ans.push_back(root->val);
+        dfs(root->right);
+    }
     vector<int> inorderTraversal(TreeNode* root) {
-    //      vector<int>v;
-    //     // inOrderTraversal(root,v);
-    //     TreeNode*temp=root;
-    //     stack<TreeNode*>st;
-    //     if(root==NULL) return v;
-    // while(true){
-    //    if(temp!=NULL){     
-    //        st.push(temp);
-    //         temp=temp->left;
-    //    }
-    //     else{
-    //         if(st.empty()) break;
-    //         temp=st.top();
-    //         st.pop();
-    //         v.push_back(temp->val);
-    //         temp=temp->right;
-    //     }
-    // }
-// return v;
-        vector<int>inorder;
-        TreeNode*curr=root;
-        while(curr!=NULL){
-            if(curr->left==NULL){
-                inorder.push_back(curr->val);
-                curr=curr->right;
-            }
-            else{
-                TreeNode*prev=curr->left;
-                while(prev->right&&prev->right!=curr){
-                  prev=prev->right;
-                }
-                if(prev->right==NULL){
-                    prev->right=curr;
-                    curr=curr->left;
-                }
-                else{
-                    prev->right=NULL;
-                    inorder.push_back(curr->val);
-                    curr=curr->right;
-                }
-            }
-        }
-        return inorder;
+           dfs(root);
+        return ans;
     }
 };
