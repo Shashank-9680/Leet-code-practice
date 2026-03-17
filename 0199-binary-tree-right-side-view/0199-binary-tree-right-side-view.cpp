1d272cffd7d9556244fdd96525b48093b23d785d
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-17 09:37:40 +0530

    Time: 0 ms (100%), Space: 15.1 MB (21.55%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -12,30 +12,26 @@
 class Solution {
 public:
     vector<int> rightSideView(TreeNode* root) {
-       vector<int>v;
-     queue<pair<TreeNode*,int>>q;
-        map<int,int>m;
-        if(root==NULL){
-            return v;
-        }
-     
-      q.push({root,0});
-      while(!q.empty()){
-         auto p=q.front();
-          TreeNode*temp=p.first;
-          q.pop();
-          int y=p.second;
-          m.insert({y,temp->val});
-          if(temp->right!=NULL) q.push({temp->right,y+1});
-            if(temp->left!=NULL) q.push({temp->left,y+1});
-            
-          
-          
-      
-      }
-       for(auto &value:m){
-            v.push_back(value.second);
-        } 
-      return v;   
+              queue<pair<TreeNode*,int>>q;
+       q.push({root,0});
+      map<int,TreeNode*>m;
+      if(root==NULL) return {};
+       while(!q.empty()){
+          int size=q.size();
+          for(int i=0;i<size;i++){
+               TreeNode*temp=q.front().first;
+           int row=q.front().second;
+           q.pop();
+           m[row]=temp;
+           if(temp->left) q.push({temp->left,row+1});
+           if(temp->right) q.push({temp->right,row+1});
+          }
+           
+       }
+       vector<int>ans;
+       for(auto it:m){
+           ans.push_back(it.second->val);
+       }
+       return ans;
     }
 };
