edf8795dbcea39d6283e0b38c40ff917ec527af7
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-27 07:57:16 +0530

    Time: 11 ms (9.81%), Space: 11.2 MB (27.25%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,31 +1,35 @@
 class Solution {
 public:
     string reverseWords(string s) {
-      vector<string>v;
-      int n=s.size();
-      int i=0;
-       while (i < n) {
-        while (i < n && s[i] == ' ') i++;
+        vector<string>ans;
+        int i=0;
+        int n=s.size();
 
-        if (i >= n) break;
-
-        int j = i;
-
-        // find word
-        while (j < n && s[j] != ' ') j++;
-
-        v.push_back(s.substr(i, j - i));
-
-        i = j;
-    }
-    reverse(v.begin(), v.end());
-
-    // build result
-    string ans = "";
-    for (int k = 0; k < v.size(); k++) {
-        ans += v[k];
-        if (k != v.size() - 1) ans += " ";
-    }
-    return ans;
+        while(i<n){
+            while(i<n&&s[i]==' '){
+                i++;
+            }
+            int j=i;
+            if(i==n) break;
+            while(i<n&&s[i]!=' '){
+                i++;
+            }
+            string temp="";
+            temp+=s.substr(j,i-j);
+            ans.push_back(temp);
+        }
+        reverse(ans.begin(),ans.end());
+        for(auto it:ans){
+            cout<<it;
+        }
+        string result="";
+        int a=ans.size();
+        for(int i=0;i<ans.size();i++){
+            result+=ans[i];
+            if(i!=a-1){
+                result+=" ";
+            }
+        }
+        return result;
     }
 };
