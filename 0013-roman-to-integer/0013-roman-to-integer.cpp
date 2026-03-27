4bb8b2ffabf8bc89996f833e054c9a78ea8a32c4
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-27 09:05:45 +0530

    Time: 11 ms (26.39%), Space: 12.5 MB (60.37%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -1,33 +1,8 @@
 class Solution {
-    int value(char a){
-        if(a=='I'){
-            return 1;
-        }
-        else if(a=='V'){
-            return 5;
-            
-        }
-        else if(a=='X'){
-            return 10;
-        }
-          else if(a=='L'){
-            return 50;
-        }
-          else if(a=='C'){
-            return 100;
-        }
-          else if(a=='D'){
-            return 500;
-        }
-          else if(a=='M'){
-            return 1000;
-        }
-    return -1;
-    }
+    
 public:
     int romanToInt(string s) {
-         unordered_map<char, int> m;
-        
+        unordered_map<int,int>m;
         m['I'] = 1;
         m['V'] = 5;
         m['X'] = 10;
@@ -35,11 +10,10 @@ public:
         m['C'] = 100;
         m['D'] = 500;
         m['M'] = 1000;
-        
-        int ans = 0;
-        
-        for(int i = 0; i < s.length(); i++){
-            if(m[s[i]] < m[s[i+1]]){
+        int ans=0;
+        for(int i=0;i<s.size();i++){
+           
+             if(m[s[i]] < m[s[i+1]]){
                 ans -= m[s[i]];
             }
             else{
@@ -47,6 +21,5 @@ public:
             }
         }
         return ans;
-       
     }
 };
