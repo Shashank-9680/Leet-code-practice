0cd98a3ccf042ceb7c06ec39cc1d6add9e4bcd85
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-27 09:03:17 +0530

    Time: 0 ms (100%), Space: 8.3 MB (85.04%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int maxDepth(string s) {
-       int ans=0;
+        int open=0;
+        int close=0;
+        int maxi=0;
         int count=0;
         for(int i=0;i<s.size();i++){
-            if(s[i]=='('){
-                count++;
-                ans=max(count,ans);
-            }
-            else if(s[i]==')'){
-                count--;
-                ans=max(count,ans);
-            }
+           if(s[i]=='('){
+            open++;
+            count++;
+           }
+           if(s[i]==')'){
+              open--;
+              maxi=max(count,maxi);
+              count--;
+            
+           }
         }
-        return ans;
+        return maxi;
     }
 };
