c1aeb0ad1f1502431df2e38978a626029df68806
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-27 07:21:24 +0530

    Time: 0 ms (100%), Space: 9 MB (23.71%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
+        int count=0;
         string ans="";
-        int count =0;
         int index=0;
-         for(int i=0;i<s.size();i++){
-              if(s[i]=='('){
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('){
                 count++;
-              }
-              if(s[i]==')'){
+            }
+             if(s[i]==')'){
                 count--;
-              }
-              if(count==0){
-                ans+=s.substr(index+1,i-index-1);
-                index=i+1;
-              }
-         }
-         return ans;
+            }
+            if(count==0){
+              ans+=s.substr(index+1,i-index-1);
+              index=i+1;
+            }
+            
+        }
+        return ans;
     }
 };
