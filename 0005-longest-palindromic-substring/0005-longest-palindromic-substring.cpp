28feab356623d3378013080c2ec65888440c6e94
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-28 09:08:28 +0530

    Time: 15 ms (53.16%), Space: 20.2 MB (37.73%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,28 +1,38 @@
 class Solution {
 public:
     string longestPalindrome(string s) {
-        string ans = "";
-        int len = 0;
-        for(int i = 0;i < s.length();i++){
-            int p1 = i,p2 = i;
-            while(p1 >= 0 && p2 < s.length() && s[p1] == s[p2]){
-                if(p2 - p1 + 1 > len){
-                    len = p2 - p1 + 1;
-                    ans = s.substr(p1,len);
-                }
-                p1--;
-                p2++;
-            }
-            p1 = i,p2 = i+1;
-            while(p1 >= 0 && p2 < s.length() && s[p1] == s[p2]){
-                if(p2 - p1 + 1 > len){
-                    len = p2 - p1 + 1;
-                    ans = s.substr(p1,len);
-                }
-                p1--;
-                p2++;
+        int n=s.size();
+        int len=0;
+        string ans;
+        for(int i=0;i<n;i++){
+            int a=i;
+            int b=i;
+            while(a>=0&&b<n&&s[a]==s[b]){
+               int length=b-a+1;
+               if(length>len){
+                   len=length;
+                   ans= s.substr(a,length);
+               }
+               a--;
+               b++;
             }
         }
+            
+            for(int i=0;i<n-1;i++){
+            int a=i;
+            int b=i+1;
+            while(a>=0&&b<n&&s[a]==s[b]){
+               int length=b-a+1;
+               if(length>len){
+                   len=length;
+                   ans= s.substr(a,length);
+               }
+               a--;
+               b++;
+            }
+        }
+
+        
         return ans;
     }
 };
