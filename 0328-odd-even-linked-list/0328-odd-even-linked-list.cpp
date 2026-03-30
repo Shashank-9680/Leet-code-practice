203216a5c61d78482a1851507d92994c195c2064
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-30 10:00:35 +0530

    Time: 0 ms (100%), Space: 15.7 MB (26.91%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -11,23 +11,18 @@
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        
-                
-  if (head == NULL || head->next == NULL) {
-            return head;
+         if(head==NULL||head->next==NULL) return head;
+        ListNode*even=head->next;
+        ListNode*odd=head;
+        ListNode*evnHead=head->next;
+        while(even!=NULL&&even->next!=NULL){
+            odd->next=even->next;
+            odd=even->next;
+            even->next=odd->next;
+            even=odd->next;
         }
-
-        ListNode* odd = head;
-        ListNode* even = head->next;
-        ListNode* evenHead = even;  
-        while (even != NULL && even->next != NULL) {
-            odd->next = even->next;
-            odd = odd->next;
-            even->next = odd->next;
-            even = even->next;
-        }
-
-        odd->next = evenHead; 
+        odd->next=evnHead;
         return head;
+        
     }
 };
