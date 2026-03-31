f6aa72c0da19b4cc3a0a0c1de8f0fe85bfb1294d
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-03-31 08:09:40 +0530

    Time: 0 ms (100%), Space: 77.2 MB (44.71%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -13,30 +13,41 @@ public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         ListNode*temp1=l1;
         ListNode*temp2=l2;
-        ListNode*newNode=new ListNode(-1);
-        ListNode*curr=newNode;
+        ListNode*newHead=new ListNode(-1);
+        ListNode*temp=newHead;
         int carry=0;
-       
-        while(temp1!=NULL||temp2!=NULL){
-          int sum=carry;
-            if(temp1) sum=sum+temp1->val;
-            if(temp2) sum=sum+temp2->val;
-            ListNode*sumNode=new ListNode(sum%10);
-           carry=sum/10;
-            curr->next=sumNode;
-            curr=sumNode;
-            if(temp1) temp1=temp1->next;
-            if(temp2) temp2=temp2->next;
-            
-            
+        while(temp1&&temp2){
+             
+             int sum=temp1->val+temp2->val+carry;
+            carry=sum/10;
+            ListNode* newNode=new ListNode(sum%10);
+            temp->next=newNode;
+            temp=newNode;
+            temp1=temp1->next;
+            temp2=temp2->next;
         }
-        if(carry==1){
-            ListNode*carryNode=new ListNode(1);
-            curr->next=carryNode;
-             curr=carryNode;
-            curr->next=nullptr;
+        while(temp1){
+            int sum=temp1->val+carry;
+            carry=sum/10;
+            ListNode* newNode=new ListNode(sum%10);
+            temp->next=newNode;
+            temp=newNode;
+            temp1=temp1->next;
         }
-        return newNode->next;
-        
+        while(temp2){
+            int sum=temp2->val+carry;
+            carry=sum/10;
+            ListNode* newNode=new ListNode(sum%10);
+            temp->next=newNode;
+            temp=newNode;
+            temp2=temp2->next;
+        }
+        if(carry){
+           ListNode*newNode=new ListNode(carry);
+           temp->next=newNode;
+           temp=newNode;
+           return newHead->next;
+        }
+        return newHead->next;
     }
 };
