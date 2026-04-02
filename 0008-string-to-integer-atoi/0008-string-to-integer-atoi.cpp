29cc39f959bc01558687f76fe704e83ed1a9d09d
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-02 09:20:27 +0530

    Time: 0 ms (100%), Space: 9.2 MB (21.53%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -4,36 +4,30 @@ public:
         int i=0;
         int n=s.size();
         int sign=1;
-        while(i<n && s[i]==' ') i++;
+        int result=0;
+        while(i<n&& s[i]==' ') i++;
+        if(i==n) return 0;
         if(s[i]=='-'){
-            sign=-1;
             i++;
+            sign=-1;
         }
         else if(s[i]=='+'){
+            i++;
             sign=1;
+        }
+        while(i<n&& s[i]=='0') i++;
+        while(i<n && isdigit(s[i])){
+            int val=s[i]-'0';
+            if(result>(INT_MAX-val)/10){
+                if(sign==1){
+                    return INT_MAX;
+                }
+                return INT_MIN;
+            }
+            result= result*10+val;
             i++;
         }
-        while(i<n&&s[i]=='0') i++;
-        int ans=0;
-        while(i<n&&isdigit(s[i])){
-           int val=s[i]-'0';
-
-           if(ans>(INT_MAX-val)/10){
-              if(sign==-1){
-                return INT_MIN;
-              }
-              else{
-                return INT_MAX;
-              }
-
-           }
-           ans=ans*10+val;
-           i++;
-        }
-        
-        if(sign<0) return -ans;
-        return ans;
-
-
+        if(sign==1) return result;
+        return -result;
     }
 };
