ab42d7aa37ce6979fe88f7a26981102691559cce
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-05 09:35:28 +0530

    Time: 0 ms (100%), Space: 22.1 MB (58.05%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.cpp
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& nums) {
+    vector<int> asteroidCollision(vector<int>& arr) {
+        int n=arr.size();
         stack<int>st;
-        vector<int>v;
-        int n=nums.size();
         for(int i=0;i<n;i++){
-            if(nums[i]>0){
-                st.push(nums[i]);
+            if(arr[i]>0){
+              st.push(arr[i]);
             }
             else{
-               
-                while (!st.empty() && st.top() > 0 && st.top() < abs(nums[i])) {
-                   
+                while(!st.empty()&&st.top()>0&&st.top()<abs(arr[i])){
                     st.pop();
                 }
-                
-                if (!st.empty() && st.top() == abs(nums[i])) {
-                    
+
+                if (!st.empty() && st.top() == abs(arr[i])) {  
                     st.pop();
                 } else if (st.empty() || st.top() < 0) {
                     
-                    st.push(nums[i]);
+                    st.push(arr[i]);
                 }
-                
             }
-          
-            
         }
-         while(!st.empty()){
-               v.push_back(st.top());
-               st.pop();
-           }
-        reverse(v.begin(),v.end());
-        return v;
-        
+        vector<int>ans;
+        while(!st.empty()){
+         ans.push_back(st.top());
+         st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
