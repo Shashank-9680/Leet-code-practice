bc569ba323fd826c4d70d6d9cfe5e9f093343e2f
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-05 09:05:13 +0530

    Time: 11 ms (15.78%), Space: 31.9 MB (5.07%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-        vector<int>newArray(nums.begin(),nums.end());
+        vector<int>nums2=nums;
         for(auto it:nums){
-            newArray.push_back(it);
+           nums2.push_back(it);
         }
+        int n=nums2.size();
+        vector<int>next(n);
         stack<int>st;
-        int n=newArray.size();
-         vector<int>nextGreater(n);
         for(int i=n-1;i>=0;i--){
-           while(!st.empty()&& newArray[i]>=st.top()){
+           while(!st.empty()&&st.top()<=nums2[i]){
             st.pop();
            }
-
-           if(st.empty()) nextGreater[i]=-1;
-           else nextGreater[i]= st.top();
-           st.push(newArray[i]);
+           if(st.empty()) next[i]=-1;
+           else next[i]=st.top();
+           st.push(nums2[i]);
         }
-       
         vector<int>ans;
         for(int i=0;i<nums.size();i++){
-         ans.push_back(nextGreater[i]);
+            ans.push_back(next[i]);
         }
         return ans;
     }
