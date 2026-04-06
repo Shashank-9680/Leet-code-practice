7185a0f676a10b002fad8ebf4c184ff10fb3df38
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-06 08:28:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Path in an Undirected Graph/shortest-path-in-an-undirected-graph.cpp
+++ Difficulty: Medium/Shortest Path in an Undirected Graph/shortest-path-in-an-undirected-graph.cpp
@@ -1,48 +1,59 @@
 class Solution {
   public:
-    vector<int> shortestPath(int N, int m, vector<vector<int>>& edges) {
-         vector<pair<int,int>>adj[N+1];
-        for(auto it:edges){
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
-        }
-      vector<int>parent(N+1);
-      for(int i=1;i<=N;i++){
-          parent[i]=i;
-      }
-      vector<int>dist(N+1,1e9);
-      dist[1]=0;
-      
-      
-      priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-      pq.push({0,1});
-      while(!pq.empty()){
-          int dis=pq.top().first;
-          int node=pq.top().second;
-          pq.pop();
-          for(auto it:adj[node]){
-              if(dist[node]+it.second<dist[it.first]){
-                  dist[it.first]=dist[node]+it.second;
-                  parent[it.first]=node;
-                  pq.push({dist[it.first],it.first});
+    vector<int> shortestPath(int V, int m, vector<vector<int>>& edges) {
+        // Code here
+         vector<pair<int,int>>adj[V+1];
+          for(auto it:edges){
+              adj[it[0]].push_back({it[1],it[2]});
+              adj[it[1]].push_back({it[0],it[2]});
+          }
+          
+          vector<int>parent(V+1);
+          vector<int>dist(V+1,INT_MAX);
+          
+          for(int i=1;i<=V;i++){
+              parent[i]=i;
+          }
+          
+          priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+          pq.push({0,1});
+          dist[1]=0;
+          
+          while(!pq.empty()){
+              int node=pq.top().second;
+              int weight=pq.top().first;
+              pq.pop();
+              for(auto it:adj[node]){
+                  if(weight+it.second<dist[it.first]){
+                      dist[it.first]=it.second+weight;
+                      pq.push({dist[it.first],it.first});
+                      parent[it.first]=node;
+                  }
               }
           }
-      }
+          
+       if(dist[V] == INT_MAX) return {-1};
+
      
-      if(dist[N]==1e9) return {-1};
+        vector<int> path;
+        int node = V;
 
+        while(parent[node] != node){
+            path.push_back(node);
+            node = parent[node];
+        }
+        path.push_back(1);
+
+        reverse(path.begin(), path.end());
+
+    
         vector<int> ans;
-        int node=N;
+        ans.push_back(dist[V]);
 
-        while(parent[node]!=node){
-            ans.push_back(node);
-            node=parent[node];
+        for(auto x : path){
+            ans.push_back(x);
         }
 
-        ans.push_back(1);
-
-        reverse(ans.begin(),ans.end());
-         ans.insert(ans.begin(), dist[N]);
-      return ans;
+        return ans;
     }
 };
