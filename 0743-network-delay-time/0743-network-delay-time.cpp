0053458eb58702397d8d006c6c50a2e22fafb604
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-06 10:02:36 +0530

    Time: 93 ms (29.16%), Space: 47.6 MB (36.83%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.cpp
+++ 0743-network-delay-time/0743-network-delay-time.cpp
@@ -1,39 +1,46 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<pair<int,int>>adj[n+1];
-        for(auto it:times){
-            adj[it[0]].push_back({it[1],it[2]});
-        }
-        vector<int>dist(n+1,1e9);
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<
-        int,int>>>pq;
-        dist[k]=0;
-        pq.push({0,k});
-        unordered_set<int>s;
-        int count=0;
-        int time=0;
-        while(!pq.empty()){
-           bool val=false;
-           int dis=pq.top().first;
-           int node=pq.top().second;
-           pq.pop();
-           if(dist[node]!=1e9){
-               s.insert(node);
-            }
-            int count=s.size();
-           if(count==n){
-              return dis;
-           }
-           for(auto it:adj[node]){
-              if(dis+it.second<dist[it.first]){
-                dist[it.first]=dis+it.second;
-                pq.push({dist[it.first],it.first});
-                val=true;
-              }
-           }
 
+        vector<pair<int,int>> adj[n+1];
+
+        for(auto it : times){
+            adj[it[0]].push_back({it[1], it[2]});
         }
-    return -1;
+
+        vector<int> dist(n+1, 1e9);
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+
+        dist[k] = 0;
+        pq.push({0, k});
+
+        while(!pq.empty()){
+            auto [dis, node] = pq.top();
+            pq.pop();
+
+            // // 🔥 Skip outdated entries
+            // if(dis > dist[node]) continue;
+
+            for(auto it : adj[node]){
+                int adjNode = it.first;
+                int wt = it.second;
+
+                if(dis + wt < dist[adjNode]){
+                    dist[adjNode] = dis + wt;
+                    pq.push({dist[adjNode], adjNode});
+                }
+            }
+        }
+
+
+        int maxi = 0;
+
+        for(int i = 1; i <= n; i++){
+            if(dist[i] == 1e9) return -1; // unreachable
+            maxi = max(maxi, dist[i]);
+        }
+
+        return maxi;
     }
 };
