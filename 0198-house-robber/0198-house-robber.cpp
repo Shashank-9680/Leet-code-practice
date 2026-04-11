ae2fa650629add2e0a61b890dda9a5ea493ce19e
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-11 10:41:46 +0530

    Time: 0 ms (100%), Space: 10.6 MB (60.24%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    int find(int i, vector<int>&nums,vector<int>&dp){
+        if(i==0) return nums[0];
+        if(i==1) return max(nums[0],nums[1]);
+        if(dp[i]!=-1) return dp[i];
+        int take= find(i-2,nums,dp)+nums[i];
+        int notTake=find(i-1,nums,dp);
+        dp[i]= max(take,notTake);
+        return dp[i];
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return find(n-1,nums,dp);
+    }
+};
