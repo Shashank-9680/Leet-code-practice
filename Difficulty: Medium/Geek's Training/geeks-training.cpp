6805df18919908e05318fa585c8dd25412ed0dd9
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-24 08:56:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -1,30 +1,25 @@
 class Solution {
     
-int find(int i, int j, vector<vector<int>>& points, vector<vector<int>>&dp){
-    if(i == 0) return points[0][j];
-
-
-    int maxi = INT_MIN;
-    if(dp[i][j]!=-1) return dp[i][j];
-    for(int k = 0; k <= 2; k++){
-        if(k != j){
-            maxi = max(maxi, points[i][j] + find(i-1, k, points,dp));
+    int find(int i,int j, vector<vector<int>>&mat,vector<vector<int>>&dp){
+        if(i==0) return mat[0][j]; 
+        if(dp[i][j]!=-1) return dp[i][j];
+         int maxi=INT_MIN;
+        for(int k=0;k<3;k++){
+            if(k!=j){
+                maxi=max(maxi,mat[i][j]+ find(i-1,k,mat,dp));
+            }
         }
+        return dp[i][j]= maxi;
     }
-    return dp[i][j]= maxi;
-}
   public:
     int maximumPoints(vector<vector<int>>& mat) {
-        // code here
         int n=mat.size();
-        int ans = 0;
+        int maxi=INT_MIN;
         vector<vector<int>>dp(n,vector<int>(3,-1));
-    for(int j = 0; j <= 2; j++){
-       
-        ans = max(ans, find(n-1, j, mat,dp));
-    }
-    return ans;
+       for(int i=0;i<3;i++){
+          maxi= max(maxi,find(n-1,i,mat,dp));
+       }
+       return maxi;
         
     }
 };
-
