163e5a45beb406a12deddd05af06446251a7c03a
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-24 08:06:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.cpp
+++ Difficulty: Medium/Frog Jump/frog-jump.cpp
@@ -2,24 +2,20 @@ class Solution {
     
     int find(int i, vector<int>&height,vector<int>&dp){
         if(i==0) return 0;
-        
         if(dp[i]!=-1) return dp[i];
-        int left= find(i-1,height,dp) + abs(height[i]-height[i-1]);
-        int right=INT_MAX;
+        int step1=find(i-1,height,dp)+abs(height[i]-height[i-1]);
+        int step2=INT_MAX;
         if(i>1){
-             right=find(i-2,height,dp)+abs(height[i]-height[i-2]);
+            step2=find(i-2,height,dp)+abs(height[i]-height[i-2]);
         }
-        
-        dp[i] = min(left,right);
+        dp[i]= min(step1,step2);
         return dp[i];
     }
   public:
     int minCost(vector<int>& height) {
         // Code here
         int n=height.size();
-        
         vector<int>dp(n,-1);
-        
         return find(n-1,height,dp);
     }
 };
