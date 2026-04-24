1fa245292420edb4366916e7c4d97407c40389c6
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-24 09:27:35 +0530

    Time: 0 ms (100%), Space: 9.5 MB (28.19%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -9,7 +9,20 @@ class Solution {
     }
 public:
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-       return count(m-1,n-1,dp); 
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        for(int i=0;i<n;i++){
+            dp[0][i]=1;
+        }
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                int count1=dp[i-1][j];
+                int count2=dp[i][j-1];
+                dp[i][j]=count1+count2;
+            }
+        }
+       return dp[m-1][n-1];
     }
 };
