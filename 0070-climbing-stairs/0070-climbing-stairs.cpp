d974772d9771a99a7e8d68fe2d28a3b4c2fd4218
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-24 07:53:03 +0530

    Time: 0 ms (100%), Space: 8.5 MB (45.81%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,21 @@
 class Solution {
-    int find(int i, vector<int>&dp){
-        if(i==0) return 1;
-        if(i==1) return 1;
-        if(dp[i]!=-1) return dp[i];
-        dp[i]= find(i-1,dp)+find(i-2,dp);
-        return dp[i];
-    }
+    // int find(int i, vector<int>&dp){
+    //     if(i==0) return 1;
+    //     if(i==1) return 1;
+    //     if(dp[i]!=-1) return dp[i];
+    //     dp[i]= find(i-1,dp)+find(i-2,dp);
+    //     return dp[i];
+    // }
 public:
     int climbStairs(int n) {
-        vector<int>dp(n+1,-1);
-        return find(n,dp);
+        vector<int>dp(n+1,0);
+       int prev1=1;
+       int prev2=1;
+        for(int i=2;i<=n;i++){
+            int curr= prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return prev2;
     }
 };
