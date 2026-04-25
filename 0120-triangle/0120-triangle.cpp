4071bac7b39faa2213f885313ae75e6483299f46
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-25 09:58:29 +0530

    Time: 0 ms (100%), Space: 13.4 MB (21.69%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,30 +1,31 @@
 class Solution {
-    int minimum(vector<vector<int>>&triangle,int i,int j,vector<vector<int>>&dp,int n){
+
+    int find(int i,int j, vector<vector<int>>&triangle,int n, vector<vector<int>>&dp){
+       
+        if(i==n-1) return triangle[n-1][j];
         if(dp[i][j]!=-1) return dp[i][j];
-        if(i==n-1){
-                return triangle[i][j];
-        }
-        
-        int down=triangle[i][j]+minimum(triangle,i+1,j,dp,n);
-        int diagonal=triangle[i][j]+minimum(triangle,i+1,j+1,dp,n);
-        return dp[i][j]=min(down,diagonal);
+        int down = triangle[i][j]+find(i+1,j,triangle,n,dp);
+        int diagonal =triangle[i][j]+find(i+1,j+1,triangle,n,dp);
+
+        return dp[i][j]= min(down,diagonal);
     }
+
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n=triangle.size();
-        vector<vector<int>>dp(n,vector<int>(n,-1));
-        // return minimum(triangle,0,0,dp,n);
-        for(int j=0;j<n;j++){
-            dp[n-1][j]=triangle[n-1][j];
-        }
+        vector<vector<int>>dp(n,vector<int>(n,0));
+        
+      for(int j=0;j<triangle[n-1].size();j++){
+         dp[n-1][j]= triangle[n-1][j];
+      }
         for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                
-                int top=triangle[i][j]+dp[i+1][j];
-                int diagonal=triangle[i][j]+dp[i+1][j+1];
-                dp[i][j]=min(top,diagonal);
+            for(int j=0;j<triangle[i].size();j++){
+                   int down = triangle[i][j]+dp[i+1][j];
+                   int diagonal =triangle[i][j]+ dp[i+1][j+1];
+
+                   dp[i][j]= min(down,diagonal);  
             }
         }
-        return dp[0][0];
+       return dp[0][0];
     }
 };
