f360f37d07c94ff2654cdf2b8d8c831afb2a4421
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-26 13:28:16 +0530

    Time: 44 ms (34.8%), Space: 48.7 MB (39.14%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -21,7 +21,29 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         if(amount==0) return 0;
         int n=coins.size();
-        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return find(n-1,amount,coins,dp)==1e9? -1:find(n-1,amount,coins,dp);
+        vector<vector<int>>dp(n,vector<int>(amount+1,1e9));
+        for(int i=1;i<n;i++){
+            dp[i][0]=0;
+        }
+          for(int t = 0; t <= amount; t++){
+             if(t % coins[0] == 0)
+            dp[0][t] = t / coins[0];
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int t = 1; t <= amount; t++){
+            
+            int notTake = dp[i-1][t];
+            
+            int take = 1e9;
+            if(coins[i] <= t){
+                take = 1 + dp[i][t - coins[i]]; // same row (unbounded)
+            }
+
+            dp[i][t] = min(take, notTake);
+        }
+    }
+        int ans = dp[n-1][amount];
+    return (ans >= 1e9) ? -1 : ans;
     }
 };
