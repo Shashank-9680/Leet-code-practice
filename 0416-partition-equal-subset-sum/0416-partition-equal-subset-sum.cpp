c9126fabf7848c08ced6a8196d54a8ef3451a585
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-26 10:19:16 +0530

    Time: 130 ms (52.1%), Space: 110.5 MB (38.01%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,29 +1,28 @@
 class Solution {
+
+    bool find(int i, int sum, vector<int>&nums, vector<vector<int>>&dp){
+        if(sum==0) return true;
+        if(i==0) return nums[0]==sum;
+        if(dp[i][sum]!=-1) return dp[i][sum];
+        bool take=false;
+        if(sum>=nums[i]){
+            take=find(i-1,sum-nums[i],nums,dp);
+        }
+        bool notTake=find(i-1,sum,nums,dp);
+        return dp[i][sum]= take||notTake;
+
+    }
 public:
     bool canPartition(vector<int>& nums) {
-       int sum=0;
-     int n=nums.size();
+        int sum=0;
+        int n=nums.size();
         for(int i=0;i<nums.size();i++){
             sum=sum+nums[i];
         }
-         if(sum%2!=0) return false;
-        int target=sum/2;
-           vector<vector<bool>>dp(n,vector<bool>(target+1,false));
        
-        for(int i=0;i<n;i++) dp[i][0]=true;
-       if (nums[0] <= target) {
-        dp[0][nums[0]] = true;
-    }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<=target;j++){
-                bool notpick=dp[i-1][j];
-                bool pick=false;
-                if(nums[i]<=j){
-                    pick=dp[i-1][j-nums[i]];
-                }
-                dp[i][j] = pick || notpick;
-            }
-        }
-        return dp[n-1][target];
+        if(sum%2!=0) return false;
+        int target=sum/2;
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
+        return find(n-1,target,nums,dp);
     }
 };
