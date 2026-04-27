259b98bde6a0fa892d9f47c200c8e7e23c38ea15
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-27 09:42:52 +0530

    Time: 0 ms (100%), Space: 10.7 MB (21.01%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,28 +1,16 @@
 class Solution {
-    // int find(int i,vector<int>&nums,vector<int>&dp){
-    //     if(i==0) return nums[0];
-    //     if(i==1) return max(nums[0],nums[1]);
-    //     if(dp[i]!=-1) return dp[i];
-    //     int take=nums[i]+find(i-2,nums,dp);
-    //     int notTake=find(i-1,nums,dp);
-    //     dp[i]= max(take,notTake);
-    //     return dp[i];
-
-    // }
+    int find(int i , vector<int>&nums, vector<int>&dp){
+        if(i==0) return nums[0];
+        if(i==1) return max(nums[0],nums[1]);
+        if(dp[i]!=-1) return dp[i];
+        int pick= nums[i]+find(i-2,nums,dp);
+        int notPick= find(i-1,nums,dp);
+        return dp[i]= max(pick,notPick);
+    }
 public:
     int rob(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,0);
-        if(n==1) return nums[0];
-        int prev1=nums[0];
-        int prev2=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            int take= nums[i]+prev1;
-            int notTake=prev2;
-            int curr=max(take,notTake);
-            prev1=prev2;
-            prev2=curr;
-        }
-       return prev2;
+        vector<int>dp(n,-1);
+        return find(n-1,nums,dp);
     }
 };
