50d7cec769c0f6286ac3093c5795b299cb5eddeb
Shashank-9680 <103242168+Shashank-9680@users.noreply.github.com>
2026-04-29 09:08:20 +0530

    Time: 27 ms (56.05%), Space: 27.4 MB (66.24%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,38 +1,38 @@
 class Solution {
-    int maximum(int n,int m,string text1,string text2,vector<vector<int>>dp){
-        if(n<0||m<0){
-            return 0;
-        }
-        if(dp[n][m]!=-1) return dp[n][m];
-        if(text1[n]==text2[m]){
-            return dp[n][m]=1+ maximum(n-1,m-1,text1,text2,dp);
-        }
-        else{
-            return dp[n][m]=max(maximum(n-1,m,text1,text2,dp),maximum(n,m-1,text1,text2,dp));
-        }
+  int find(int i, int j, string text1, string text2,vector<vector<int>>&dp){
+    if(i==0||j==0) return 0;
+    if(dp[i][j]!=-1) return dp[i][j];
+    if(text1[i-1]==text2[j-1]){
+       return dp[i][j]= 1+find(i-1,j-1,text1,text2,dp);  
     }
+    int take1=find(i-1,j,text1,text2,dp);
+    int take2=find(i,j-1,text1,text2,dp);
+    return dp[i][j]= max(take1,take2);
+  }
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int n=text1.size();
         int m=text2.size();
         vector<vector<int>>dp(n+1,vector<int>(m+1,0));
-        // return maximum(n-1,m-1,text1,text2,dp);
-      
-
-        // Fill the DP table iteratively
-        for (int i = 0; i <=n; i++) {
-            for (int j = 0; j <=m; j++) {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = 0; // Base case initialization
-                } else if (text1[i - 1] == text2[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
+        }
+        for(int i=0;i<=m;i++){
+            dp[0][i]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                  dp[i][j]= 1+dp[i-1][j-1];
                 }
+                else{
+                    int take1=dp[i-1][j];
+                    int take2=dp[i][j-1];
+                    dp[i][j]=max(take1,take2);
+                }
+               
             }
         }
-
-        // The answer is in dp[n][m]
         return dp[n][m];
     }
 };
